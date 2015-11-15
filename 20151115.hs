d3e73b8d7ba723e1b89e3dca62de97d8de157940
Q.P.Liu <qpliu@yahoo.com>
2015-11-15 08:29:08 -0800

    Minor cleanup.

--- 20151115.hs
+++ 20151115.hs
@@ -1,21 +1,19 @@
 import Data.Set(Set,elems,fromList,member)
 
-replacements :: String -> Maybe [String]
+replacements :: String -> [String]
 replacements word = rep "" word
   where
     rep pre (a:b:c:rest) | succ a == b && succ b == c = reps (reverse pre) rest
                          | otherwise = rep (a:pre) (b:c:rest)
-    rep _ _ = Nothing
-    reps pre post = Just (map ((pre++) . (:post)) ['a'..'z'])
+    rep _ _ = []
+    reps pre post = map ((pre++) . (:post)) ['a'..'z']
 
-answers :: Set String -> String -> Maybe (String,[String])
-answers words word =
-    fmap ((,) word . filter (`member` words)) (replacements word)
+answers :: Set String -> String -> (String,[String])
+answers words word = ((,) word . filter (`member` words) . replacements) word
 
-display :: Maybe (String,[String]) -> IO ()
-display Nothing = return ()
-display (Just (word,replacers)) | null replacers = return ()
-                                 | otherwise = print (word,replacers)
+display :: (String,[String]) -> IO ()
+display (word,replacers) | null replacers = return ()
+                         | otherwise = print (word,replacers)
 
 main :: IO ()
 main = do

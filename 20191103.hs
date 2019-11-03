9d7359646fa022d2f34c020f9e0ff958f76d76b1
Q.P.Liu <qpliu@yahoo.com>
2019-11-03 09:58:47 -0800

    Add dequeue, beety, agee, uey, ziti.  Output unique results.

--- 20191103.hs
+++ 20191103.hs
@@ -1,7 +1,7 @@
 import Data.Set(fromList,member)
 
 validPair :: String -> Bool
-validPair = flip member (fromList ["ez", "ne", "nv", "nq", "dk", "bd", "ad", "at", "cd", "kg", "sa", "vu", "qd", "qt", "ic", "iv"])
+validPair = flip member (fromList ["ez", "ne", "nv", "nq", "dk", "dq", "bd", "bt", "ad", "at", "ag", "cd", "kg", "sa", "vu", "qd", "qt", "ic", "iv", "ue", "zd", "zt"])
 
 initialValidPairs :: String -> [(String,String)]
 initialValidPairs "" = []
@@ -9,15 +9,13 @@ initialValidPairs (c:cs) = filter (validPair . snd) (makePairs c cs "")
 
 makePairs :: Char -> String -> String -> [(String,String)]
 makePairs ch "" rest = []
-makePairs ch (c:cs) rest =
-    (cs++rest,[ch,c]):(cs++rest,[c,ch]):makePairs ch cs (c:rest)
+makePairs ch (c:cs) rest = (cs++rest,[ch,c]):(cs++rest,[c,ch]):makePairs ch cs (c:rest)
 
 search :: (String,[String]) -> [[String]]
 search ("",pairs) = [pairs]
-search (str,pairs) =
-    concatMap search (map (fmap (:pairs)) (initialValidPairs str))
+search (str,pairs) = concatMap search (map (fmap (:pairs)) (initialValidPairs str))
 
 main :: IO ()
-main = mapM_ print (search ("endbackstagetvquiz",[]))
+main = (mapM_ print . fromList . map fromList . search) ("endbackstagetvquiz",[])
 
 -- ["ez","qt","vu","ic","kg","at","sa","bd","ne"]

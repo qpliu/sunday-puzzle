7f99d0cd6d0c1edaa568782c5a147b8fd4152f69
Q.P.Liu <qpliu@yahoo.com>
2020-05-22 12:45:39 -0700

    Add riddler 20200522.

---
+++ riddler/2020/20200522c.hs
@@ -0,0 +1,37 @@
+import Data.Char(toLower,isAlpha)
+import Data.Foldable(maximumBy)
+import Data.Map(Map,alter,empty,toList)
+import Data.Set(Set,fromList,intersection,member)
+import qualified Data.Set
+
+states :: [(String,Set Char)]
+states = map f ["Alaska", "Hawaii", "Washington", "Oregon", "California",
+       "Nevada", "Idaho", "Montana", "Wyoming", "Utah", "Colorado", "Arizona",
+       "New Mexico", "Texas", "Oklahoma", "Kansas", "Nebraska", "South Dakota",
+       "North Dakota", "Minnesota", "Iowa", "Missouri", "Arkansas",
+       "Louisiana", "Missisippi", "Alabama", "Georgia", "Florida",
+       "South Carolina", "North Carolina", "Virginia", "West Virginia",
+       "Tennessee", "Kentucky", "Wisconsin", "Michigan", "Illinois", "Indiana",
+       "Ohio", "Pennsylvania", "Maryland", "Delaware", "New Jersey",
+       "New York", "Connecticut", "Rhode Island", "Massachusetts", "Vermont",
+       "New Hampshire", "Maine"]
+  where f s = (s,(fromList . map toLower . filter isAlpha) s)
+
+noShareds :: String -> [String]
+noShareds word = (map fst . filter noShared) states
+  where
+    letters = (fromList . map toLower . filter isAlpha) word
+    noShared (_,state) = Data.Set.null (intersection letters state)
+
+mackerel :: String -> Bool
+mackerel word = length (noShareds word) == 1
+
+collect :: Map String Int -> String -> Map String Int
+collect counts word
+  | mackerel word = alter (Just . maybe 1 (+ 1)) (head (noShareds word)) counts
+  | otherwise = counts
+
+main :: IO ()
+main = do
+  readFile "word.list" >>= print . maximum . map (\ w -> (length w,w,head (noShareds w))) . filter mackerel . filter ((> 20) . length) . lines
+  readFile "word.list" >>= print . maximumBy (\ a b -> compare (snd a) (snd b)) . toList . foldl collect empty . lines

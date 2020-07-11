4f3911ce3662cf14b40d43fa5ee963f00f2a2bdd
Q.P.Liu <qpliu@yahoo.com>
2020-07-11 10:14:35 -0700

    Add additional riddler 20200710 express answer.

--- riddler/2020/20200710x.hs
+++ riddler/2020/20200710x.hs
@@ -12,7 +12,7 @@ apply _ _ Nothing = Nothing
 apply (Op _ op) (Just a) (Just b) = op a b
 
 ops :: [Op]
-ops = [Op "+" (tryOp (+)),Op "-" (tryOp (-)),Op "*" (tryOp (*)),Op "/" tryDiv,Op "^" tryExp]
+ops = [Op "+" (tryOp (+)),Op "-" (tryOp (-)),Op "*" (tryOp (*)),Op "/" tryDiv,Op "^" tryExp,Op "f-" (tryOp (flip (-))),Op "f/" (flip tryDiv),Op "f^" (flip tryExp)]
   where
     tryOp :: (Rational -> Rational -> Rational)  -> Rational -> Rational -> Maybe Rational
     tryOp op = \ a b -> Just (op a b)
@@ -24,12 +24,12 @@ ops = [Op "+" (tryOp (+)),Op "-" (tryOp (-)),Op "*" (tryOp (*)),Op "/" tryDiv,Op
     tryExp a b
       | denominator b /= 1 = Nothing
       | numerator b < 0 && numerator a == 0 = Nothing
-      | numerator b < 0 = Just ((1 % 1) / pow a (- numerator b))
-      | otherwise = Just (pow a (numerator b))
-    pow :: Rational -> Integer -> Rational
+      | numerator b < 0 = fmap ((1 % 1) /) (pow a (- numerator b))
+      | otherwise = pow a (numerator b)
+    pow :: Rational -> Integer -> Maybe Rational
     pow a b
-      | b > 1 = a * pow a (b - 1)
-      | otherwise = a
+      | b > 100 = Nothing
+      | otherwise = Just (product (take (fromIntegral b) (repeat a)))
 
 applySeq :: (Op,Op,Op) -> (Maybe Rational,Maybe Rational,Maybe Rational,Maybe Rational) -> Maybe Rational
 applySeq (x,y,z) (a,b,c,d) = apply z (apply y (apply x a b) c) d
@@ -38,7 +38,7 @@ applyPairs :: (Op,Op,Op) -> (Maybe Rational,Maybe Rational,Maybe Rational,Maybe 
 applyPairs (x,y,z) (a,b,c,d) = apply z (apply x a b) (apply y c d)
 
 search :: [((Op,Op,Op),(Maybe Rational,Maybe Rational,Maybe Rational,Maybe Rational),Maybe Rational,Maybe Rational)]
-search = [(opseq,numseq,applySeq opseq numseq,applyPairs opseq numseq) | opseq <- opseqs, numseq <- numseqs,applySeq opseq numseq == Just (24%1)]
+search = [(opseq,numseq,applySeq opseq numseq,applyPairs opseq numseq) | opseq <- opseqs, numseq <- numseqs,applySeq opseq numseq == Just (24%1) || applyPairs opseq numseq == Just (24%1)]
   where
     opseqs = [(x,y,z) | x <- ops, y <- ops, z <- ops]
     numseqs = [(a,b,c,d) | [a,b,c,d] <- permutations [Just (2%1),Just (3%1),Just (3%1),Just (4%1)]]

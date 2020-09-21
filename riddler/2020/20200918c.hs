d0f4e2dde9b43c307ad8c33e35661b68a53f2cb7
Q.P.Liu <qpliu@yahoo.com>
2020-09-21 14:52:48 -0700

    Make code slightly cleaner.

--- riddler/2020/20200918c.hs
+++ riddler/2020/20200918c.hs
@@ -1,22 +1,22 @@
 import Data.Ratio((%))
 
-expected :: Rational -> Integer -> Integer -> Rational
-expected chance guessNumber guessOptions
+expected :: Integer -> Integer -> Rational
+expected guessNumber guessOptions
   | guessOptions == 0 = 0
   | guessOptions `mod` 2 == 1 =
-      chance * (guessNumber % guessOptions)
-          + expected (chance * ((guessOptions - 1) % guessOptions))
-                     (guessNumber + 1) (guessOptions `div` 2)
+      guessNumber % guessOptions
+          + ((guessOptions - 1) % guessOptions)
+              * expected (guessNumber + 1) (guessOptions `div` 2)
   | otherwise =
-      chance * (guessNumber % guessOptions)
-          + expected (chance * ((guessOptions `div` 2) % guessOptions))
-                     (guessNumber + 1) (guessOptions `div` 2)
-          + expected (chance * ((guessOptions `div` 2 - 1) % guessOptions))
-                     (guessNumber + 1) (guessOptions `div` 2 - 1)
+      guessNumber % guessOptions
+          + ((guessOptions `div` 2) % guessOptions)
+              * expected (guessNumber + 1) (guessOptions `div` 2)
+          + ((guessOptions `div` 2 - 1) % guessOptions)
+              * expected (guessNumber + 1) (guessOptions `div` 2 - 1)
 
 main :: IO ()
 main = do
     print answer
     print (fromRational answer :: Double)
   where
-    answer = expected 1 1 267751
+    answer = expected 1 267751

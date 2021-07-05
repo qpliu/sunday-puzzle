952b7c29d7908f74dfc5a1bc4084470609326569
Q.P.Liu <qpliu@yahoo.com>
2021-07-05 09:22:52 -0700

    Finish calculation for riddler classic 20210702. Fix incorrect answer for riddler express 20210702.

--- riddler/2021/20210702c.hs
+++ riddler/2021/20210702c.hs
@@ -14,21 +14,26 @@ joinDots dots a b = start dots []
       | dot == target = (reverse dots ++ collected,opposite)
       | otherwise = continue dots collected target (dot:opposite)
 
-pairs :: [a] -> [(a,a)]
+pairs :: Ord a => [a] -> [(a,a)]
 pairs [] = []
 pairs [_] = []
-pairs (a:as) = map ((,) a) as ++ pairs as
+pairs [a,b] = [(a,b)]
+pairs as = p as ++ p (filter (/= maximum as) as)
+  where p as = map ((,) (maximum as)) (filter (/= maximum as) as)
 
-maxScore :: (Num a, Ord a) => [a] -> a
-maxScore [] = 0
-maxScore [_] = 0
-maxScore [a,b] = a*b
+maxScore :: (Num a, Ord a) => [a] -> (a,[(a,a)])
+maxScore [] = (0,[])
+maxScore [_] = (0,[])
+maxScore [a,b] = (a*b,[(a,b)])
 maxScore dots =
-    maximum [a*b + maxSubscores (joinDots dots a b) | (a,b) <- pairs dots]
+    maximum [maxSubscores a b (joinDots dots a b) | (a,b) <- pairs dots]
   where
-    maxSubscores (loop1,loop2) = maxScore loop1 + maxScore loop2
+    maxSubscores a b (loop1,loop2) = (a*b+s1+s2,(a,b):p1++p2)
+      where
+        (s1,p1) = maxScore loop1
+        (s2,p2) = maxScore loop2
 
-search :: Int -> Int -> (Int,[Int]) -> [[Int]] -> IO ()
+search :: Int -> Int -> ((Int,[(Int,Int)]),[Int]) -> [[Int]] -> IO ()
 search blocksize n best arrangements
   | null arrangements = return ()
   | otherwise = do
@@ -41,7 +46,20 @@ main :: IO ()
 main = do
     print (maxScore [1..11],250)
     print (maxScore [1,4,8,7,11,2,5,9,3,6,10],237)
-    search 1000 0 (250,[]) (map (1:) (filter twobeforethree (permutations [2..11])))
+    search 1000 0 ((250,[]),[]) (map (1:) (filter twobeforethree (permutations [2..11])))
+    -- search 1000 1600000 ((224,[]),[1,4,2,8,5,10,7,11,6,9,3]) (map (1:) (drop 1600000 (filter twobeforethree (permutations [2..11])))) -- skip the first 60 hours of searching
+    -- non-exhaustive list of arrangements with max score of 224:
+    --  [1,8,5,10,7,11,6,9,4,2,3]
+    --  [1,9,6,10,7,11,5,8,4,2,3]
+    --  [1,4,2,8,5,10,7,11,6,9,3]
+    --  [1,4,2,8,5,11,7,10,6,9,3]
+    --  [1,4,2,9,6,10,7,11,5,8,3]
+    --  [1,6,11,7,10,5,9,4,8,2,3]
+    --  [1,7,10,6,11,5,9,4,8,2,3]
+    --  [1,8,4,9,5,10,7,11,6,2,3]
+    --  [1,8,4,9,5,10,6,11,7,2,3]
+    --  [1,4,2,3,8,5,10,7,11,6,9]
+    --  [1,4,2,3,9,6,10,7,11,5,8]
   where
     twobeforethree [] = True
     twobeforethree (2:_) = True

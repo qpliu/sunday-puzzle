21999e8b341b23e7ded7e7d72074f2ce7a687815
Q.P.Liu <qpliu@yahoo.com>
2021-07-22 07:55:49 -0700

    Corrections for riddler classic 20210716 and note on why I think my answer may still be wrong.

--- riddler/2021/20210716c.hs
+++ riddler/2021/20210716c.hs
@@ -5,17 +5,16 @@ avgShots :: Rational -> Int -> Int -> Int -> Rational
 avgShots p round score1 score2
   | round > 5 = if score1 == score2 then n6 p else 0
   | 6-round < abs (score1-score2) = 0
-  | 6-round == abs (score1-score2) =
-    -- first shot in the round could end it
-    -- end on score if the leading team goes first
-      1/2*p*1 +
-    -- end on miss if the trailing team goes first
-      1/2*(1-p)*1 +
-    -- otherwise both shots are taken in the round
-      1/2*p*p*(2 + avgShots p (round+1) (score1+1) (score2+1)) +
-      1/2*(1-p)*(1-p)*(2 + avgShots p (round+1) score1 score2) +
-      1/2*p*(1-p)*(2 + avgShots p (round+1) (score1+1) score2) +
-      1/2*(1-p)*p*(2 + avgShots p (round+1) score1 (score2+1))
+  | 6-round == score1-score2 =
+    -- end on score when the leading team goes first
+      p*1 +
+      (1-p)*p*(2 + avgShots p (round+1) score1 (score2+1)) +
+      (1-p)*(1-p)*(2 + avgShots p (round+1) score1 score2)
+  | 6-round == score2-score1 =
+    -- end on miss when the trailing team goes first
+      (1-p)*1 +
+      p*p*(2 + avgShots p (round+1) (score1+1) (score2+1)) +
+      p*(1-p)*(2 + avgShots p (round+1) (score1+1) score2)
   | otherwise =
       p*p*(2 + avgShots p (round+1) (score1+1) (score2+1)) +
       (1-p)*(1-p)*(2 + avgShots p (round+1) score1 score2) +
@@ -25,4 +24,4 @@ avgShots p round score1 score2
 main :: IO ()
 main = do
     print $ let a = avgShots (7/10) 1 0 0 in (fromRational a :: Double, a)
-    mapM_ print [let a = avgShots (fromIntegral n/10) 1 0 0 in (fromRational a :: Double,a,n) | n <- [1..9]]
+    mapM_ print [let a = avgShots p 1 0 0 in (p,fromRational a :: Double,a,fromRational (a - n6 p) :: Double) | p <- [1/10,2/10..9/10] ++ [95/100,99/100,999/1000,9999/10000,1/10000]]

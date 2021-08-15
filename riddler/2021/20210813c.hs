8d75ce47d7c09a8655f691bc64e1cdc1945cb0d6
Q.P.Liu <qpliu@yahoo.com>
2021-08-15 09:53:23 -0700

    Make code for riddler classic 20210813 a little faster.

--- riddler/2021/20210813c.hs
+++ riddler/2021/20210813c.hs
@@ -1,15 +1,20 @@
-import Data.List(sort)
+import Data.List(nub,permutations)
 
-combos :: Int -> [[Int]]
-combos 0 = [[]]
-combos n = concat [map (r:) (combos (n-1)) | r <- [1..6]]
+combos :: Int -> [(Int,[Int])]
+combos 0 = [(0,[])]
+combos n = concat [map (addCount . (r:)) (combos' (n-1) r) | r <- [1..6]]
+  where
+    combos' :: Int -> Int -> [[Int]]
+    combos' 0 _ = [[]]
+    combos' n' m = concat [map (r:) (combos' (n'-1) r) | r <- [1..m]]
+    addCount d = ((length . nub . permutations) d,d)
 
 -- There's certainly some elegant expression from combinatorics that would
 -- make this much faster but an ugly brute force should suffice
 s :: Int -> Rational
 s 0 = 0
 s 1 = 21/6
-s n = sum [sn n (reverse (sort d)) | d <- combos n] / (6^fromIntegral n)
+s n = sum [fromIntegral c * sn n d | (c,d) <- combos n] / (6^fromIntegral n)
 
 sn :: Int -> [Int] -> Rational
 sn n (d1:d) = fromIntegral d1 + sn' (n-1) d

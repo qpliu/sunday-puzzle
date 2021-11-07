a5e1009ae174dedb07f97cd94a7778c251b5ec3c
Q.P.Liu <qpliu@yahoo.com>
2021-11-07 12:24:39 -0800

    Add more thoughts on riddler classic 20211105.

--- riddler/2021/20211105c.hs
+++ riddler/2021/20211105c.hs
@@ -11,4 +11,31 @@ sumFactors n = s 0 1
       where q = n `div` m
 
 main :: IO ()
-main = mapM_ print [i | i <- [1..50000], cm i == sumFactors i]
+main = do
+    mapM_ print [i | i <- [1..50000], cm i == sumFactors i]
+    mapM_ print (take 6 construct)
+
+data QueueItem a = QueueItem ([QueueItem a] -> a)
+
+dequeue :: [QueueItem a] -> a
+dequeue [] = error "dqueue"
+dequeue (QueueItem f:queue) = f queue
+
+construct :: [Integer]
+construct = search 2 (254/100 - 1) 1 [] []
+  where
+    search :: Integer -> Rational -> Integer -> [[Integer]] -> [QueueItem [Integer]] -> [Integer]
+    search m a b sieve queue
+      | a <= 0 = dequeue queue
+      | any ((==m) . head) sieve = dequeue (queue ++ [QueueItem (search (m+1) a b nextSieve)])
+      | length ns > 0 = ns ++ dequeue nextQueue
+      | length nPerfectSquares > 0 = nPerfectSquares ++ dequeue nextQueue
+      | otherwise = dequeue nextQueue
+      where
+        nextQueue = queue ++ [QueueItem (search (m+1) a b ([2*m,3*m..]:nextSieve)),
+                              QueueItem (search (m+1) (a - 1/fromIntegral m) (b + m) nextSieve)]
+        nextSieve = map (dropWhile (<= m)) sieve
+        ns :: [Integer]
+        ns = [n | n <- [ceiling ((fromIntegral b - 1/2)/a) .. floor ((fromIntegral b + 1/2)/a)], n > 1, n `mod` m == 0, m <= n `div` m, cm n == sumFactors n]
+        nPerfectSquares :: [Integer]
+        nPerfectSquares = [n | n <- [ceiling ((fromIntegral b - fromIntegral m - 1/2)/a) .. floor ((fromIntegral b - fromIntegral m + 1/2)/a)], n > 1, n `mod` m == 0, m <= n `div` m, cm n == sumFactors n]

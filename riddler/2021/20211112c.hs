927affb2b886a39a235c8181cf7b35296a22e8b0
Q.P.Liu <qpliu@yahoo.com>
2021-11-14 12:10:29 -0800

    More thoughts on riddler classic 20211112.

--- riddler/2021/20211112c.hs
+++ riddler/2021/20211112c.hs
@@ -1,8 +1,7 @@
 w :: (Rational -> Rational -> Rational) -> Rational -> Rational -> Rational
 w strategy stickLength geniesMinimumStickLength
-  | f <= 2*g = (f-g)^2/(4*(1-g))
+  | f <= 2*g || t*f <= g = (f-g)^2/(4*(1-g))
   | t <= 0 || t >= 1 = error ("invalid strategy: t=" ++ show t ++ ", f=" ++ show f ++ ", g=" ++ show g)
-  | t*f <= g = 0
   | t >= 1/2 = (t*f-g)/(1-g) * (1-t)*f
   | otherwise = (t*f-g)/(1-g) * (1-t)*f + (1 - (t*f-g)/(1-g))*w strategy ((1-t)*f) (t*f)
   where
@@ -12,4 +11,6 @@ w strategy stickLength geniesMinimumStickLength
 
 main :: IO ()
 main = do
-    mapM_ print [let x = w (const (const fraction)) 1 0 in (fraction,x,fromRational x :: Double) | fraction <- [9990/30000,9991/30000..10010/30000]]
+    mapM_ print [let x = w (strategy fraction) 1 0 in (fraction,x,fromRational x :: Double) | fraction <- [1/30,2/30..11/30]]
+  where
+    strategy d f g = (d+g)/f

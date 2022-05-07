df3ef334406db0178ee699f27f2e60dbcaca1707
Q.P.Liu <qpliu@yahoo.com>
2022-05-07 10:20:17 -0700

    Find inconclusive answer for riddler classic 20220506 extra credit.

--- riddler/2022/20220506c.hs
+++ riddler/2022/20220506c.hs
@@ -1,7 +1,6 @@
-import Data.Foldable(maximumBy)
-import Data.Map(Map,mapWithKey,(!))
+import Data.Map(Map,keys,mapWithKey,(!))
 import qualified Data.Map
-import Data.Set(Set,difference,union,unions)
+import Data.Set(Set,difference,intersection,union,unions)
 import qualified Data.Set
 
 primes :: [Int]
@@ -47,13 +46,46 @@ formatHit (n,(_,_,_:l)) = (n,formatList l (const True))
     formatList [] _ = []
     formatList (i:l) f = let j = head (filter f (Data.Set.toList i)) in j : formatList l (oneStep j)
 
+connectedSubgraph :: Map Int (Set Int) -> Int -> Set Int
+connectedSubgraph graph n = collect [n] Data.Set.empty
+  where
+    collect [] set = set
+    collect (n:ns) set
+      | Data.Set.member n set = collect ns set
+      | otherwise = collect (ns++Data.Set.toList (graph!n)) (Data.Set.insert n set)
+
+findLadder :: Map Int (Set Int) -> Int -> Int -> [Int]
+findLadder graph a b = search (initAset,initAset,[]) (initBset,initBset,[])
+  where
+    initAset = Data.Set.fromList [a]
+    initBset = Data.Set.fromList [b]
+    search (atops,aset,alist) (btops,bset,blist)
+      | not (Data.Set.null abMeets) = reverse (tail (thread abMeet alist)) ++ abMeet:tail (thread abMeet blist)
+      | otherwise = search (btops,bset,blist) (newAtops,union newAtops aset,atops:alist)
+      where
+        newAtops = difference (unions (map (graph!) (Data.Set.toList atops))) aset
+        abMeets = intersection atops btops
+        abMeet = head (Data.Set.toList abMeets)
+        thread n [] = [n]
+        thread n (set:sets) = n : thread (head (filter (oneStep n) (Data.Set.toList set))) sets
+
+findMaxLadder :: Map Int (Set Int) -> Int -> (Int,[Int])
+findMaxLadder graph maxConnections =
+    maximum [let ladder = findLadder graph a b in (length ladder,ladder) | a <- starts, b <- starts, b > a && not (oneStep a b)]
+  where
+    starts = keys (Data.Map.filter (\ connections -> let n = Data.Set.size connections in n > 0 && n <= maxConnections) graph)
+
 main :: IO ()
 main = do
     p 10
     p 100
     p 1000
     p 10000
-    p 100000
-    p 1000000
+    p2 10 5
+    p2 100 5
+    p2 1000 5
+    p2 10000 4
+    p2 100000 1
   where
     p n = print $ formatHit $ findAMax $ makeGraph $ takeWhile (< 10*n) $ dropWhile (< n) primes
+    p2 n m = print $ findMaxLadder (makeGraph $ takeWhile (< 10*n) $ dropWhile (< n) primes) m

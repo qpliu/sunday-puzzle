c38484d1cc84ac829eb38d681b42df979beca271
Q.P.Liu <qpliu@yahoo.com>
2022-06-05 09:57:00 -0700

    Faster code for riddler express 20220603.

--- riddler/2022/20220603x.hs
+++ riddler/2022/20220603x.hs
@@ -1,18 +1,17 @@
-import Data.Set(Set,delete,fromList,member,toList)
+import Data.Set(Set,fromList,member,toList)
 
-data Expr = C Int | Plus Expr Expr | Times Expr Expr deriving Show
+splits :: [a] -> [([a],[a])]
+splits [] = []
+splits [a,b] = [([a],[b])]
+splits (a:as) = ([a],as) : concat [[(a:xs,ys),(xs,a:ys)] | (xs,ys) <- splits as]
 
-make :: Set Int -> Int -> [(Expr,Set Int)]
-make numbers target
-  | null numbers = []
-  | target `member` numbers = (C target,delete target numbers) : submakes
-  | otherwise = submakes
-  where
-    submakes =
-        [(Plus expr1 expr2,ns2) | l <- [1 .. target `div` 2], (expr1,ns1) <- make numbers l, (expr2,ns2) <- make ns1 (target - l)] ++
-        [(Times expr1 expr2,ns2) | l <- [1 .. target `div` 2], target `mod` l == 0, (expr1,ns1) <- make numbers l, (expr2,ns2) <- make ns1 (target `div` l)]
+makes :: [Int] -> Set Int
+makes [] = fromList []
+makes [a] = fromList [a]
+makes as = fromList $ concat [[x,y,x+y,x*y] | (xs,ys) <- splits as, x <- toList (makes xs), y <- toList (makes ys)]
 
-fun n = search (sum [1..n]+1)
-  where
-    search i | null (make (fromList [1..n]) i) = i
-             | otherwise = search (i+1)
+fun :: Int -> Int
+fun n = head $ filter (not . flip member (makes [1..n])) [1..]
+
+main :: IO ()
+main = mapM_ print [(n,fun n) | n <- [1..8]]

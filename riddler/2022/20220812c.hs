5b7dc143e4481f9e6b2811bebdb5459bd0d01ed5
Q.P.Liu <qpliu@yahoo.com>
2022-08-12 17:24:36 -0700

    Add code for riddler classic 20220812 that seems to say my answer is wrong.

---
+++ riddler/2022/20220812c.hs
@@ -0,0 +1,5 @@
+bet :: Double -> Double -> Double -> (Double,Double) -> (Double,Double)
+bet goal house fraction (previousSuccess,money) =
+    (previousSuccess+thisSuccess,money*(1-fraction))
+  where
+    thisSuccess = (1-previousSuccess)*house*fraction*money/goal

4b80afef1eb9a0fd0ae549c6b9ed11c61082bb89
Q.P.Liu <qpliu@yahoo.com>
2022-08-13 11:28:26 -0700

    Add code for riddler express 20220812 that does xomino decomposition.

--- riddler/2022/20220812x.hs
+++ riddler/2022/20220812x.hs
@@ -1,4 +1,4 @@
-import Data.Set(Set,fromList,insert,member,toList)
+import Data.Set(Set,difference,fromList,insert,isSubsetOf,member,partition,size,toList)
 import qualified Data.Set
 
 smap :: Ord b => (a -> b) -> Set a -> Set b
@@ -18,8 +18,24 @@ addSquare set = toList $ fromList $ map canonical [insert (x,y) set | x <- [mini
 addSquares :: [Set (Int,Int)] -> [Set (Int,Int)]
 addSquares sets = toList $ fromList $ concatMap addSquare sets
 
+xrange :: Set (Int,Int) -> [Int]
+xrange set = [minimum (smap fst set) .. maximum (smap fst set)]
+
+yrange :: Set (Int,Int) -> [Int]
+yrange set = [minimum (smap snd set) .. maximum (smap snd set)]
+
 showXomino :: Set (Int,Int) -> String
-showXomino set = unlines [[if (x,y) `member` set then '#' else ' ' | x <- [minimum (smap fst set) .. maximum (smap fst set)]] | y <- [minimum (smap snd set) .. maximum (smap snd set)]]
+showXomino set = unlines [[if (x,y) `member` set then '#' else ' ' | y <- yrange set] | x <- xrange set]
+
+decomposeable :: Int -> Set (Int,Int) -> Bool
+decomposeable n set
+  | n == 0 = True
+  | n == 1 = size set > 0
+  | otherwise = or [decomposeable (n-1) (difference set array) | x <- xrange set, y <- yrange set, array <- [fromList [(x+i,y) | i <- [0..n-1]],fromList [(x,y+i) | i <- [0..n-1]]], array `isSubsetOf` set]
 
 main :: IO ()
-main = mapM_ (\ (i,s) -> print i >> putStr s) $ zip [1..] $ map showXomino $ head $ drop 5 $ iterate addSquares [fromList [(0,0)]]
+main = do
+    mapM_ (\ s -> print (decomposeable 3 s) >> putStr (showXomino s)) $ head $ drop 5 $ iterate addSquares [fromList [(0,0)]]
+    (\ (a,b) -> print (size b,size a+size b)) $ partition (decomposeable 2) $ fromList $ head $ drop 2 $ iterate addSquares [fromList [(0,0)]]
+    (\ (a,b) -> print (size b,size a+size b)) $ partition (decomposeable 3) $ fromList $ head $ drop 5 $ iterate addSquares [fromList [(0,0)]]
+    (\ (a,b) -> print (size b,size a+size b)) $ partition (decomposeable 4) $ fromList $ head $ drop 9 $ iterate addSquares [fromList [(0,0)]]

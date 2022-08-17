be8c031702084d3bf4e90a0c6a03a55ff8b6eab7
Q.P.Liu <qpliu@yahoo.com>
2022-08-16 18:04:27 -0700

    Correction to code for riddler classic 20220812.

--- riddler/2022/20220812c.hs
+++ riddler/2022/20220812c.hs
@@ -2,4 +2,4 @@ bet :: Double -> Double -> Double -> (Double,Double) -> (Double,Double)
 bet goal house fraction (previousSuccess,money) =
     (previousSuccess+thisSuccess,money*(1-fraction))
   where
-    thisSuccess = (1-previousSuccess)*house*fraction*money/goal
+    thisSuccess = (1-previousSuccess)*house*fraction*money/(goal-money*(1-fraction))

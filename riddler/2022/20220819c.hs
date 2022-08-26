f09b0820eaa471e131387c931274c548c245c718
Q.P.Liu <qpliu@yahoo.com>
2022-08-26 09:27:39 -0700

    Minor code cleanup for riddler classic 20220819.

--- riddler/2022/20220819c.hs
+++ riddler/2022/20220819c.hs
@@ -7,9 +7,7 @@ p1 ((half,full),prob) = fromList $ filter (\ ((h,f),p) -> h >= 0 && f >= 0 && p 
     ((half-1,full-1),prob*(divide (2*half*full) ((half+full)*(half+full-1)) + if half < 2 then 0 else divide (half*(half-1)*full) ((half+full)*(half+full-1)*(half+full-2))))
     ]
   where
-    divide a b
-      | a == 0 && b == 0 = 1
-      | otherwise = fromIntegral a/fromIntegral b
+    divide a b = fromIntegral a/fromIntegral b
 
 p :: Map (Int,Int) Rational -> Map (Int,Int) Rational
 p ps = (unionsWith (+) . map p1 . toList) ps

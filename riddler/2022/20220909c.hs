8cbd038a7ad6423bcb0c1086cdda1908804ad456
Q.P.Liu <qpliu@yahoo.com>
2022-09-11 17:53:23 -0700

    Better extra credit answer for riddler classic 20220909.

--- riddler/2022/20220909c.hs
+++ riddler/2022/20220909c.hs
@@ -1,12 +1,3 @@
-a5 :: Double -> Double
-a5 r = 1 - 2*sqrt(r^2-1/16)
-
-b5 :: Double -> Double
-b5 r = 1 - 4*sqrt(r^2-(a5 r)^2/4)
-
-f5 :: Double -> Double
-f5 r = r + sqrt(r^2 - (b5 r)^2/4) - a5 r
-
 bisect :: (Double -> Double) -> Double -> Double -> Double -> Double
 bisect f epsilon x1 x2
   | abs (x2 - x1) < epsilon = x3
@@ -15,70 +6,77 @@ bisect f epsilon x1 x2
   | otherwise = error "error"
   where x3 = (x1 + x2)/2
 
-h6 :: Double -> Double
-h6 r = 1 - 2*r/sqrt 2
-
-a6 :: Double -> Double
-a6 r = 1/2 - 2*r/sqrt 2
-
-b6 :: Double -> Double
-b6 r = 1/2 - 2*sqrt(r^2 - (h6 r)^2/4)
-
-x6 :: Double -> Double
-x6 r = 1/2 + (a6 r - b6 r)/2
-
-y6 :: Double -> Double
-y6 r = sqrt(r^2 - (a6 r + b6 r)^2/4)
-
-f6 :: Double -> Double
-f6 r = y6 r + sqrt(r^2 - (x6 r - r*sqrt 2)^2) - 1 + r*sqrt 2
-
-xyi6 :: Double -> (Double,Double)
-xyi6 r = (xa + r*cos(theta+dtheta),ya + r*sin(theta+dtheta))
+f5 :: Double -> (Double,[(Double,Double)])
+f5 r = (f,[(x1,y1),(1-x1,y1),(x2,y2),(1-x2,y2),(x3,y3)])
   where
-    xa = x6 r
-    ya = 1 - y6 r
-    xb = 1 - x6 r
-    yb = y6 r
-    theta = atan((yb-ya)/(xb-xa))
-    dtheta = acos(sqrt((xa-xb)^2+(ya-yb)^2)/(2*r))
+    x1 = 1/4
+    y1 = 1 - sqrt(r^2 - x1^2)
+    y2 = (1-2*(1-y1))/2
+    x2 = sqrt(r^2 - y2^2)
+    w = 1 - 4*x2
+    x3 = 1/2
+    y3 = sqrt(r^2 - w^2/4)
+    f = y3+r - (1 - 2*sqrt(r^2 - x1^2))
 
-f6_2 :: Double -> Double
-f6_2 r = r - sqrt((xi-x0)^2 + (yi-y0)^2)
+f6a :: Double -> (Double,[(Double,Double)])
+f6a r = (f,[(x1,y1),(1-x1,1-y1),(x2,y2),(y2,x2),(x3,y3),(y3,x3)])
   where
-    (xi,yi) = xyi6 r
-    x0 = 1 - r/sqrt 2
-    y0 = 1 - r/sqrt 2
+    x1 = r/sqrt 2
+    y1 = r/sqrt 2
+    y2 = 1-(1-2*y1)/2
+    x2 = sqrt(r^2 - (1-y2)^2)
+    x3 = (2*x2 + (1 - 2*x1))/2
+    w = 1 - 2*x1 - 2*x2
+    y3 = 1 - sqrt(r^2 - w^2/4)
+    f = y3 - sqrt(r^2 - (x3 - 2*x1)^2) - 2*y1
+
+f6b :: Double -> (Double,[(Double,Double)])
+f6b r = (f,[(x1,y1),(1-x1,1-y1),(x2,y2),(1-x2,1-y2),(x3,y3),(1-x3,1-y3)])
+  where
+    x1 = r/sqrt 2
+    y1 = r/sqrt 2
+    y2 = 1-(1-2*y1)/2
+    x2 = sqrt(r^2 - (1-y2)^2)
+    x3 = (2*x2 + (1 - 2*x1))/2
+    w = 1 - 2*x1 - 2*x2
+    y3 = 1 - sqrt(r^2 - w^2/4)
+    theta = atan((1-y3-y3)/(1-x3-x3))
+    dtheta = acos(sqrt((1-x3-x3)^2+(1-y3-y3)^2)/(2*r))
+    xi = x3 + r*cos(theta+dtheta)
+    yi = y3 + r*sin(theta+dtheta)
+    f = r - sqrt((1-x1-xi)^2+(1-y1-yi)^2)
+
+f6c :: Double -> Double -> (Double,[(Double,Double)])
+f6c phi r = (f,[(x1,y1),(1-x1,1-y1),(x2,y2),(1-x2,1-y2),(x3,y3),(1-x3,1-y3)])
+  where
+    x1 = r*cos phi
+    y1 = r*sin phi
+    y2 = 1-(1-2*y1)/2
+    x2 = sqrt(r^2 - (1-y2)^2)
+    x3 = (2*x2 + (1 - 2*x1))/2
+    w = 1 - 2*x1 - 2*x2
+    y3 = 1 - sqrt(r^2 - w^2/4)
+    theta = atan((1-y3-y3)/(1-x3-x3))
+    dtheta = acos(sqrt((1-x3-x3)^2+(1-y3-y3)^2)/(2*r))
+    xi = x3 + r*cos(theta+dtheta)
+    yi = y3 + r*sin(theta+dtheta)
+    f = r - sqrt((1-x1-xi)^2+(1-y1-yi)^2)
+
+gen :: (Double -> (Double,[(Double,Double)])) -> Double -> Double -> Double -> String
+gen f epsilon r1 r2 = svg rmin ((snd . f) rmin)
+  where
+    rmin = bisect (fst . f) epsilon r1 r2
+
+svg :: Double -> [(Double,Double)] -> String
+svg r circles = "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n<svg version=\"1.1\" xmlns=\"http://www.w3.org/2000/svg\" width=\"400\" height=\"400\" viewBox=\"-50 -50 200 200\">\n<rect x=\"0\" y=\"0\" width=\"100\" height=\"100\" fill=\"transparent\" stroke=\"black\"/>\n" ++ concatMap circle circles ++ "</svg>\n"
+  where
+    circle (x,y) = "<circle cx=\"" ++ show (100*x) ++ "\" cy=\"" ++ show (100*y) ++ "\" r=\"" ++ show (100*r) ++ "\" fill=\"transparent\" stroke=\"black\"/>"
 
 main :: IO ()
 main = do
-  let r5 = bisect f5 0.0000001 0.32 0.4
-  print ("r5",r5)
-  print (1/4,1-sqrt(r5^2-1/16))
-  print (3/4,1-sqrt(r5^2-1/16))
-  let x5 = sqrt(r5^2-(a5 r5)^2/4)
-  print (x5,sqrt(r5^2-x5^2))
-  print (1-x5,sqrt(r5^2-x5^2))
-  print (1/2,1-r5-2*sqrt(r5^2-1/16))
-  let r6 = bisect f6 0.0000001 0.30 0.33
-  print ("r6",r6)
-  print (r6/sqrt 2,r6/sqrt 2)
-  print (1-r6/sqrt 2,1-r6/sqrt 2)
-  let x6a = (1-2*r6/sqrt 2)/2
-  let y6a = sqrt(r6^2-x6a^2)
-  print (1-x6a,y6a)
-  print (y6a,1-x6a)
-  print (x6 r6,1-y6 r6)
-  let r6_2 = bisect f6_2 0.0000001 0.301 0.308
-  print ("r6-2",r6_2)
-  print (r6_2/sqrt 2,r6_2/sqrt 2)
-  print (1-r6_2/sqrt 2,1-r6_2/sqrt 2)
-  let x6_2a = (1-2*r6_2/sqrt 2)/2
-  let y6_2a = sqrt(r6_2^2-x6_2a^2)
-  print (1-y6_2a,x6_2a)
-  print (y6_2a,1-x6_2a)
-  print (x6 r6_2,1-y6 r6_2)
-  print (1-x6 r6_2,y6 r6_2)
-
-  let (xi,yi) = xyi6 r6_2
-  print (xi,yi)
+  putStr (gen f5 0.0000001 0.30 0.35)
+  putStr (gen f6a 0.0000001 0.30 0.35)
+  putStr (gen f6b 0.0000001 0.301 0.31)
+  let (r6c,phi6c) = minimum [(bisect (fst . f6c (pi/4+phi)) 0.0000001 0.288 0.3,phi+pi/4) | phi <- [0.125,0.12505 .. 0.135]]
+  print (r6c,phi6c,phi6c*180/pi)
+  putStr (gen (f6c phi6c) 0.0000001 0.288 0.3)

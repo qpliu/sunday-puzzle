6adf97e76f1aa8be6b9d02dd04ae59f287e7133e
Q.P.Liu <qpliu@yahoo.com>
2023-10-06 10:17:48 -0700

    Add further thoughts on fiddler 20231006 extra credit.

--- riddler/2023/20231006.hs
+++ riddler/2023/20231006.hs
@@ -9,3 +9,15 @@ l = [(n,(n8,n7,n6,n5,n4,n3,n2,n1)) | n8 <- [0..8],
          n1 <- [0..1], n1 == 0 || (n1 /= n8 && n1 /= n7 && n1 /= n6 && n1 /= n5 && n1 /= n4 && n1 /= n2),
          n <- [n8*product [1..8] + n7*product [1..7] + n6*product [1..6] + n5*product [1..5] + n4*product [1..4] + n3*product [1..3] + n2*product [1..2] + n1],
          n `mod` 2520 == 0]
+
+l2 :: [(Integer,(Integer,Integer,Integer,Integer,Integer,Integer,Integer,Integer))]
+l2 = [(n,(n8,n7,n6,n5,n4,n3,n2,n1)) | n8 <- [0..8],
+         n7 <- [0..7],
+         n6 <- [0..6],
+         n5 <- [0..5],
+         n4 <- [0..4],
+         n3 <- [0..3],
+         n2 <- [0..2],
+         n1 <- [0..1],
+         n <- [n8*product [1..8] + n7*product [1..7] + n6*product [1..6] + n5*product [1..5] + n4*product [1..4] + n3*product [1..3] + n2*product [1..2] + n1],
+         n `mod` 2520 == 0]

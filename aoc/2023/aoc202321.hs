d8e46fcfd623f6261c8d1779b52f38ad20ddd76d
Q.P.Liu <qpliu@yahoo.com>
2023-12-21 09:17:34 -0800

    Get the right answer for Advent of Code 2023 day 21.  Still has a bug that makes it fail with the example input, which, by having rocks in the starting row and starting column, is more difficult than my input, which has no rocks in the starting row and starting column.

--- aoc/2023/aoc202321.hs
+++ aoc/2023/aoc202321.hs
@@ -117,7 +117,7 @@ walkFrom garden startXYs = go Data.Map.empty startXYs
       where
         continue (x,y) (dx,dy)
           | not (member (x+dx,y+dy) garden) = []
-          | Data.Map.member (x+dx,y+dy) dists = []
+          | Data.Map.member (x+dx,y+dy) dists && dists!(x+dx,y+dy) <= dist+1 = []
           | otherwise = [((x+dx,y+dy),dist+1)]
 
 countReachable :: Int -> Int -> Map (Int,Int) Int -> Int
@@ -126,9 +126,9 @@ countReachable nsteps stepsToEdge distsFromEdge =
   where
     isReachable distFromEdge = stepsToEdge + distFromEdge <= nsteps && odd (stepsToEdge + distFromEdge) == odd nsteps
 
-countDir :: Set (Int,Int) -> (Int,Int) -> Int -> [((Int,Int),(Int,Int))] -> Bool -> Int -> Int
-countDir garden startXY tileSize toNextTileXYs linear nsteps =
-    traceShow (toNextTileXYs,linear,nsteps,fullyReachableM,reachableM)
+countDir :: Set (Int,Int) -> (Int,Int) -> Int -> [((Int,Int),(Int,Int))] -> Bool -> Int -> String -> Int
+countDir garden startXY tileSize toNextTileXYs linear nsteps debugTag =
+    --traceShow (debugTag,toNextTileXYs,linear,nsteps,fullyReachableM,reachableM)
     fullyReachableN + partiallyReachableN
   where
     tileDists = walkFrom garden [(nextTileStartXY,((walkFrom garden [(startXY,0)])!exitStartTileXY) + (if linear then 1 else 2)) | (exitStartTileXY,nextTileStartXY) <- toNextTileXYs]
@@ -140,80 +140,92 @@ countDir garden startXY tileSize toNextTileXYs linear nsteps =
     fullyReachableN :: Int
     fullyReachableN
       | linear = (countReachable nsteps 0 tileDists)*((fullyReachableM+1) `div` 2) + (countReachable nsteps tileSize tileDists)*(fullyReachableM `div` 2)
-      | otherwise = (countReachable nsteps 0 tileDists)*(fullyReachableM `div` 2)^2 + (countReachable nsteps tileSize tileDists)*(let i = fullyReachableM`div` 2 in (2*i+2)*i`div`2)
+      | otherwise = (countReachable nsteps 0 tileDists)*((fullyReachableM+1) `div` 2)^2 + (countReachable nsteps tileSize tileDists)*(let i = fullyReachableM `div` 2 in (i+1)*i)
 
     partiallyReachableN
       | linear = sum [countReachable nsteps (m*tileSize) tileDists | m <- [fullyReachableM .. reachableM]]
-      | otherwise = sum [countReachable nsteps (m*tileSize) tileDists*m | m <- [fullyReachableM .. reachableM]]
+      | otherwise = sum [countReachable nsteps (m*tileSize) tileDists*(m+1) | m <- [fullyReachableM .. reachableM]]
 
 result2 :: String -> Int
 result2 input = 
-    countDir garden startXY 131 [((65,0),    (65,130))]  True  nsteps + -- N
-    countDir garden startXY 131 [((0,0),     (130,130))] False nsteps + -- NW
-    countDir garden startXY 131 [((0,65),    (130,65))]  True  nsteps + -- W
-    countDir garden startXY 131 [((0,130),   (130,0))]   False nsteps + -- SW
-    countDir garden startXY 131 [((65,130),  (65,0))]    True  nsteps + -- S
-    countDir garden startXY 131 [((130,130), (0,0))]     False nsteps + -- SE
-    countDir garden startXY 131 [((130,65),  (0,65))]    True  nsteps + -- E
-    countDir garden startXY 131 [((130,0),   (0,130))]   False nsteps + -- NE
+    countDir garden startXY 131 [((65,0),    (65,130))]  True  nsteps "N" +
+    countDir garden startXY 131 [((0,0),     (130,130))] False nsteps "NW" +
+    countDir garden startXY 131 [((0,65),    (130,65))]  True  nsteps "W" +
+    countDir garden startXY 131 [((0,130),   (130,0))]   False nsteps "SW" +
+    countDir garden startXY 131 [((65,130),  (65,0))]    True  nsteps "S" +
+    countDir garden startXY 131 [((130,130), (0,0))]     False nsteps "SE" +
+    countDir garden startXY 131 [((130,65),  (0,65))]    True  nsteps "E" +
+    countDir garden startXY 131 [((130,0),   (0,130))]   False nsteps "NE" +
     countReachable nsteps 0 (walkFrom garden [(startXY,0)])
   where
     nsteps = 26501365
     (garden,startXY) = parse input
 
--- 592712209696441 is too low
-
+-- will give the wrong answer for small number of steps
+-- because it goes to the corners for going to the next tile to the N, E, W, S
+-- there's also still an off-by-1 bug when going farther
+-- 6 - 16
+-- 10 - 33 - should be 50 (undercount is expected)
+-- 50 - 1592 - should be 1594
+-- 100 - 6534 - should be 6536
+-- 500 - 176002 - should be 167004
+-- 1000 - 668693 - should be 668697
+-- 5000 - 16733042 - should be 16733044
 testResult2 :: Int -> [Int]
 testResult2 nsteps = 
-    [countDir garden startXY 11 [((0,0),(0,10)),((10,0),(10,10))] True nsteps, -- N
-    countDir garden startXY 11 [((0,0),(10,10))] False nsteps, -- NW
-    countDir garden startXY 11 [((0,0),(10,0)),((0,10),(10,10))] True  nsteps, -- W
-    countDir garden startXY 11 [((0,10),(10,0))] False nsteps, -- SW
-    countDir garden startXY 11 [((0,10),(0,0)),((10,10),(0,10))] True  nsteps, -- S
-    countDir garden startXY 11 [((10,10),(0,0))] False nsteps, -- SE
-    countDir garden startXY 11 [((10,0),(0,0)),((10,10),(0,10))] True  nsteps, -- E
-    countDir garden startXY 11 [((10,0),(0,10))] False nsteps, -- NE
+    [countDir garden startXY 11 [((0,0),(0,10)),((10,0),(10,10))] True nsteps "N",
+    countDir garden startXY 11 [((0,0),(10,10))] False nsteps "NW",
+    countDir garden startXY 11 [((0,0),(10,0)),((0,10),(10,10))] True nsteps "W",
+    countDir garden startXY 11 [((0,10),(10,0))] False nsteps "SW",
+    countDir garden startXY 11 [((0,10),(0,0)),((10,10),(0,10))] True nsteps "S",
+    countDir garden startXY 11 [((10,10),(0,0))] False nsteps "SE",
+    countDir garden startXY 11 [((10,0),(0,0)),((10,10),(0,10))] True nsteps "E",
+    countDir garden startXY 11 [((10,0),(0,10))] False nsteps "NE",
     countReachable nsteps 0 (walkFrom garden [(startXY,0)]),
     0]
   where
     (garden,startXY) = parse testData
 
-testData2 :: String
-testData2 = unlines [
-    ".................................",
-    ".....###.#......###.#......###.#.",
-    ".###.##..#..###.##..#..###.##..#.",
-    "..#.#...#....#.#...#....#.#...#..",
-    "....#.#........#.#........#.#....",
-    ".##...####..##...####..##...####.",
-    ".##..#...#..##..#...#..##..#...#.",
-    ".......##.........##.........##..",
-    ".##.#.####..##.#.####..##.#.####.",
-    ".##..##.##..##..##.##..##..##.##.",
-    ".................................",
-    ".................................",
-    ".....###.#......###.#......###.#.",
-    ".###.##..#..###.##..#..###.##..#.",
-    "..#.#...#....#.#...#....#.#...#..",
-    "....#.#........#.#........#.#....",
-    ".##...####..##..S####..##...####.",
-    ".##..#...#..##..#...#..##..#...#.",
-    ".......##.........##.........##..",
-    ".##.#.####..##.#.####..##.#.####.",
-    ".##..##.##..##..##.##..##..##.##.",
-    ".................................",
-    ".................................",
-    ".....###.#......###.#......###.#.",
-    ".###.##..#..###.##..#..###.##..#.",
-    "..#.#...#....#.#...#....#.#...#..",
-    "....#.#........#.#........#.#....",
-    ".##...####..##...####..##...####.",
-    ".##..#...#..##..#...#..##..#...#.",
-    ".......##.........##.........##..",
-    ".##.#.####..##.#.####..##.#.####.",
-    ".##..##.##..##..##.##..##..##.##.",
-    "................................."
-    ]
+tileDir :: Int -> (Int,Int) -> (Set (Int,Int),(Int,Int)) -> (Set (Int,Int),(Int,Int))
+tileDir nTile (dx,dy) (garden,startXY) =
+    (fromList $ concatMap tile $ toList garden,startXY)
+  where
+    tile (x,y) = [(x+i*dx,y+j*dy) | i <- [0..nTile], j <- [0..nTile]] 
+
+testN :: Int -> Int -> IO ()
+testN nTile nsteps = do
+    (garden,startXY) <- fmap parse $ readFile "input/21.txt"
+    print startXY
+    print (startXY,"untiled",p1 (garden,startXY),countReachable nsteps 0 (walkFrom garden [(startXY,0)]))
+    print ("tiled",p1 $  tileDir nTile (0,-131) (garden,startXY),
+        countDir garden startXY 131 [((65,0),    (65,130))]  True  nsteps "N" +
+        countReachable nsteps 0 (walkFrom garden [(startXY,0)]))
+  where
+    p1 (garden,startXY) = length $ filter (reachable nsteps startXY) $ toList $ walk nsteps (garden,startXY)
+
+testW :: Int -> Int -> IO ()
+testW nTile nsteps = do
+    (garden,startXY) <- fmap parse $ readFile "input/21.txt"
+    print startXY
+    print (startXY,"untiled",p1 (garden,startXY),countReachable nsteps 0 (walkFrom garden [(startXY,0)]))
+    print ("tiled",p1 $  tileDir nTile (-131,0) (garden,startXY),
+        countDir garden startXY 131 [((0,65),    (130,65))]  True  nsteps "W" +
+        countReachable nsteps 0 (walkFrom garden [(startXY,0)]))
+  where
+    p1 (garden,startXY) = length $ filter (reachable nsteps startXY) $ toList $ walk nsteps (garden,startXY)
+
+testNW :: Int -> Int -> IO ()
+testNW nTile nsteps = do
+    (garden,startXY) <- fmap parse $ readFile "input/21.txt"
+    print startXY
+    print (startXY,"untiled",p1 (garden,startXY),countReachable nsteps 0 (walkFrom garden [(startXY,0)]))
+    print ("tiled",p1 $  tileDir nTile (-131,-131) (garden,startXY),
+        countDir garden startXY 131 [((65,0),    (65,130))]  True  nsteps "N" +
+        countDir garden startXY 131 [((0,0),     (130,130))] False nsteps "NW"+
+        countDir garden startXY 131 [((0,65),    (130,65))]  True  nsteps "W" +
+        countReachable nsteps 0 (walkFrom garden [(startXY,0)]))
+  where
+    p1 (garden,startXY) = length $ filter (reachable nsteps startXY) $ toList $ walk nsteps (garden,startXY)
 
 test2 :: ()
 test2
@@ -223,3 +235,6 @@ part2 :: IO Int
 part2 = fmap result2 $ readFile "input/21.txt"
 
 -- 592712209696441 is too low
+-- 592718082010011 is too low
+-- 592723929202499 is too low
+-- 592723929260582 is the right answer

6b68877205b4e23524cd6accbaaf7446b3c0605c
Q.P.Liu <qpliu@yahoo.com>
2023-12-27 13:37:24 -0800

    More tools for exploring Collatz series.

--- riddler/2023/20231222.hs
+++ riddler/2023/20231222.hs
@@ -12,6 +12,9 @@ collatz n
   | odd n = 3*n + 1
   | otherwise = n `div` 2
 
+collatzSeries :: Integer -> [Integer]
+collatzSeries = takeWhile (/= 1) . iterate collatz
+
 inverseCollatz :: Integer -> [Integer]
 inverseCollatz n
   | (n-1) `mod` 3 == 0 && odd ((n-1) `div` 3) = [(n-1) `div` 3,2*n]
@@ -19,3 +22,25 @@ inverseCollatz n
 
 sources :: [Integer] -> [Integer]
 sources = sort . concatMap inverseCollatz . filter ((/= 0) . (`mod` 3))
+
+data T a = M a | T a (T a) (T a) | Z deriving Show
+
+instance Foldable T where
+  foldr f b t = foldr f b (flatten t)
+
+tree :: Int -> Integer -> T Integer
+tree maxDepth n
+  | maxDepth <= 0 = Z
+  | otherwise =
+      case n `mod` 6 of
+        0 -> M (n `div` 2)
+        3 -> M n
+        4 -> T (n `div` 3) (tree (maxDepth-1) (2*n)) (tree (maxDepth-1) (n `div` 3))
+        _ -> tree (maxDepth-1) (2*n)
+
+flatten :: T a -> [a]
+flatten t = f [] t
+  where
+    f l (M n) = n:l
+    f l (T n t1 t2) = f (f (n:l) t1) t2
+    f l Z = l

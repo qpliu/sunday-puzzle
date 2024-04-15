dfa47b42e78762a82f93b69c920041433ddf9bbd
Q.P.Liu <qpliu@yahoo.com>
2024-04-15 14:10:11 -0700

    Start working on N=5 for fiddler 20240412 extra credit.

---
+++ riddler/2024/20240412ec.hs
@@ -0,0 +1,73 @@
+
+bisect :: Double -> (Double -> Double) -> Double -> Double -> Double
+bisect epsilon f x0 x1
+  | abs(x0 - x1) < epsilon = x
+  | y*y0 < 0 = bisect epsilon f x0 x
+  | y*y1 < 0 = bisect epsilon f x x1
+  | otherwise = error (show ((x0,y0),(x,y),(x1,y1)))
+  where
+    x = (x0+x1)/2
+    (y,y0,y1) = (f x,f x0,f x1)
+
+a :: Double -> Double
+a alpha = 2*sin(alpha/2)/sqrt 3
+
+b :: Double -> Double
+b alpha = 2*cos(alpha/2)/3
+
+d :: Double -> Double -> Double
+d alpha beta = 2*aa^2*bb*cc/(bb^2*ss^2 + aa^2*cc^2)
+  where
+    gamma = (alpha + beta)/2
+    aa = a alpha
+    bb = b alpha
+    cc = cos(gamma/2)
+    ss = sin(gamma/2)
+
+alpha5a :: Double -> Double
+alpha5a epsilon = bisect epsilon f (pi/10) (pi/5)
+  where
+    f alpha = cc^2/aa^2 + (ss - bb - dd)^2/bb^2 - 1
+      where
+        beta = pi - 3*alpha/2
+        aa = a alpha
+        bb = b alpha
+        dd = d alpha beta
+        cc = cos(beta/2)
+        ss = sin(beta/2)
+
+alpha5b :: Double -> Double
+alpha5b epsilon = bisect epsilon f (pi/10) (pi/5)
+  where
+    f alpha = cc^2/bb^2 + (ss - aa - dd)^2/aa^2 - 1
+      where
+        beta = pi - 3*alpha/2
+        aa = a alpha
+        bb = b alpha
+        dd = d alpha beta
+        cc = cos(beta/2)
+        ss = sin(beta/2)
+
+alpha6a :: Double -> Double
+alpha6a epsilon = bisect epsilon f (pi/12) (pi/6)
+  where
+    f alpha = cc^2/aa^2 + (ss - bb - dd)^2/bb^2 - 1
+      where
+        beta = pi/3 - alpha
+        aa = a alpha
+        bb = b alpha
+        dd = d alpha beta
+        cc = cos(beta/2)
+        ss = sin(beta/2)
+
+alpha6b :: Double -> Double
+alpha6b epsilon = bisect epsilon f (pi/12) (pi/6)
+  where
+    f alpha = cc^2/bb^2 + (ss - aa - dd)^2/aa^2 - 1
+      where
+        beta = pi/3 - alpha
+        aa = a alpha
+        bb = b alpha
+        dd = d alpha beta
+        cc = cos(beta/2)
+        ss = sin(beta/2)

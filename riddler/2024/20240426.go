6b578f11a93ddb96b52384e39c413c0a738baf96
Q.P.Liu <qpliu@yahoo.com>
2024-05-01 11:11:53 -0700

    Add check of expressions for probabilities against simulation results.

--- riddler/2024/20240426.go
+++ riddler/2024/20240426.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"math"
 	"math/rand"
 )
 
@@ -43,7 +44,7 @@ func ectrial(x, y float64, r *rand.Rand) bool {
 		}
 	}
 	b := r.Float64()
-	if b > a {
+	if b > a && b > x {
 		return false
 	}
 	if b < a || b < x {
@@ -102,23 +103,29 @@ func ftsimulate(x float64, n int, r *rand.Rand) int {
 }
 
 func main() {
-	const n = 1000000
-	const seed = 806112731
+	const n = 10000000
+	const seed = 80611273192
 	r := rand.New(rand.NewSource(seed))
 	xs := []float64{0.50,0.5321,0.55,0.5701,0.60,0.65}
 	for _, x := range xs {
 		m := simulate(x, n, r)
-		fmt.Printf("x=%f %d/%d %f\n", x, m, n, float64(m)/n)
+		sp := float64(m)/n
+		cp := 1-(math.Pow(x,4)+4*math.Pow(x,3)-4*x+8)/12
+		fmt.Printf("x=%f %d/%d %f %f %f\n", x, m, n, sp, cp, math.Abs(sp-cp)/cp)
 	}
 	ys := []float64{0.60,0.64865,0.65,0.70}
 	for _, x := range xs {
 		for _, y := range ys {
 			m := ecsimulate(x, y, n, r)
-			fmt.Printf("x=%f y=%f %d/%d %f\n", x, y, m, n, float64(m)/n)
+			sp := float64(m)/n
+			cp := 1-(-5*math.Pow(x,6)+4*math.Pow(y,6)+24*math.Pow(y,5)-24*y+96)/120
+			fmt.Printf("x=%f y=%f %d/%d %f %f %f\n", x, y, m, n, sp, cp, math.Abs(sp-cp)/cp)
 		}
 	}
 	for _, x := range xs {
 		m := ftsimulate(x, n, r)
-		fmt.Printf("x=%f %d/%d %f\n", x, m, n, float64(m)/n)
+		sp := float64(m)/n
+		cp := 1-(math.Exp(2*x)*(x-2) + math.Exp(x)*(x+math.E-1) + 1)
+		fmt.Printf("x=%f %d/%d %f %f %f\n", x, m, n, sp, cp, math.Abs(sp-cp)/cp)
 	}
 }

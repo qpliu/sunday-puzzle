cd7eaa84bd1fe549f0397183b2555dd991e04e21
Q.P.Liu <qpliu@yahoo.com>
2024-08-06 15:34:18 -0700

    Fix Monte Carlo calculation for fidder 20240802 extra credit.

--- riddler/2024/20240802.go
+++ riddler/2024/20240802.go
@@ -3,9 +3,10 @@ package main
 import (
 	"fmt"
 	"math/rand"
+	"sort"
 )
 
-func trial(n int, s, t, tmp []int, r *rand.Rand) bool {
+func trial(n int, bruteForceOrder bool, s, t, tmp []int, r *rand.Rand) (bool, bool) {
 	for i := range n {
 		s[i] = i
 	}
@@ -23,54 +24,86 @@ func trial(n int, s, t, tmp []int, r *rand.Rand) bool {
 			nCandidates++
 		}
 	}
-	if nCandidates == 0 {
-		return false
-	}
 	for i := range n {
 		t[i] = i
 	}
 	r.Shuffle(n, func(i, j int) {
 		t[i], t[j] = t[j], t[i]
 	})
+
+	gap := false
 	for _, i := range tmp[:nCandidates] {
-		// if s[winner]-s[i] = 3, need 1
-		// if s[winner]-s[i] = 4, need 1
-		// if s[winner]-s[i] = 5, need 2
-		// if s[winner]-s[i] = 6, need 2
-		nAfterBoth := (s[winner]-s[i]-1)/2
-		count := 0
+		countBetween := 0
 		bothFinish := max(t[i], t[winner])
 		for j := winner+1; j < i; j++ {
-			if t[j] > bothFinish {
-				count++
-				if count >= nAfterBoth {
-					return true
+			if t[j] < bothFinish {
+				countBetween++
+			}
+		}
+		if s[winner] > 2 + 2*countBetween + s[i] {
+			gap = true
+			break
+		}
+	}
+
+	bruteForce := false
+	if bruteForceOrder {
+		copy(tmp, t)
+		for i, j := range tmp {
+			t[j] = i
+		}
+		for i := range n {
+			sort.Ints(t[:i+1])
+			haveWinner := winner == t[0]
+			leader := t[0]
+			leaderScore := s[leader]
+			for j := range i+1 {
+				if t[j] == winner {
+					haveWinner = true
 				}
+				if 2*j + s[t[j]] < leaderScore {
+					leader = t[j]
+					leaderScore = 2*j + s[t[j]]
+				}
+			}
+			if haveWinner && leader != winner {
+				bruteForce = true
+				break
 			}
 		}
 	}
-	return false
+
+	return gap, bruteForce
 }
 
-func simulate(ntrials, n int, r *rand.Rand) int {
+func simulate(ntrials, n int, bruteForceOrder bool, r *rand.Rand) (int, int) {
 	s := make([]int, n)
 	t := make([]int, n)
 	tmp := make([]int, n)
-	count := 0
+	countGap := 0
+	countBruteForce := 0
 	for _ = range ntrials {
-		if trial(n, s, t, tmp, r) {
-			count++
+		gap, bruteForce := trial(n, bruteForceOrder, s, t, tmp, r)
+		if gap {
+			countGap++
+		}
+		if bruteForce {
+			countBruteForce++
 		}
 	}
-	return count
+	return countGap, countBruteForce
 }
 
 func main() {
-	const ntrials = 500000
-	const seed = 73462098
+	const ntrials = 100000
+	const seed = 73462091
 	r := rand.New(rand.NewSource(seed))
-	for _, n := range([]int{3, 4, 5, 50, 500, 5000}) {
-		count := simulate(ntrials, n, r)
-		fmt.Printf("N=%d %d/%d %f\n", n, count, ntrials, float64(count)/float64(ntrials))
+	for _, n := range([]int{3, 4, 5, 6, 8, 10, 50, 500, 5000, 10000}) {
+		bruteForceOrder := false
+		countGap, countBruteForce := simulate(ntrials, n, bruteForceOrder, r)
+		if bruteForceOrder {
+			fmt.Printf("brute force N=%d %d/%d %f\n", n, countBruteForce, ntrials, float64(countBruteForce)/float64(ntrials))
+		}
+		fmt.Printf("N=%d %d/%d %f\n", n, countGap, ntrials, float64(countGap)/float64(ntrials))
 	}
 }

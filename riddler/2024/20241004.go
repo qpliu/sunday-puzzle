6c7964f9f44eca26be4eebe0e910d5415d542dcd
Q.P.Liu <qpliu@yahoo.com>
2024-10-05 18:31:02 -0700

    Get answer for N=10 for fiddler 20241004 extra credit.

--- riddler/2024/20241004.go
+++ riddler/2024/20241004.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"math/rand"
+	"os"
 	"runtime"
 )
 
@@ -59,15 +60,82 @@ func simulate(n, ncpu, nDice int, r *rand.Rand) {
 	fmt.Printf("%d: %f %f\n", nDice, float64(totals[0])/float64(totals[1]), float64(totals[2])/float64(totals[3]))
 }
 
+func pdistTrial(nDice int, dist [][6]int, r *rand.Rand) {
+	rolls := map[int]int{0: 0, 1: 0, 2: 0, 3: 0, 4: 0, 5: 0}
+	for _ = range nDice {
+		rolls[r.Intn(6)]++
+	}
+	firstCount := 0
+	numCount := 0
+	for _, count := range rolls {
+		if count > firstCount {
+			firstCount = count
+			numCount = 1
+		} else if count == firstCount {
+			numCount++
+		}
+	}
+	dist[firstCount-1][numCount-1]++
+}
+
+func pdist(n, ncpu, nDice int, r *rand.Rand) {
+	ch := make(chan [][6]int)
+	for _ = range ncpu {
+		go func(r *rand.Rand) {
+			dist := make([][6]int, nDice)
+			for _ = range n {
+				pdistTrial(nDice, dist, r)
+			}
+			ch <- dist
+		}(rand.New(rand.NewSource(int64(r.Uint64()))))
+	}
+	totals := make([][6]int, nDice)
+	for _ = range ncpu {
+		dist := <-ch
+		for i := range dist {
+			for j := range dist[i] {
+				totals[i][j] += dist[i][j]
+			}
+		}
+	}
+	for i, tots := range totals {
+		tot := 0
+		isSplit := false
+		for k, count := range tots {
+			if count > 0 {
+				fmt.Printf("%d %d,%d: %g\n", nDice, i+1, k+1, float64(count)/float64(n*ncpu))
+				if tot > 0 {
+					isSplit = true
+				}
+				tot += count
+			}
+		}
+		if isSplit {
+			fmt.Printf("%d %d: %g\n", nDice, i+1, float64(tot)/float64(n*ncpu))
+		}
+	}
+}
+
 func main() {
 	const seed = 20241004
 	r := rand.New(rand.NewSource(seed))
-	const n = 100000
 	ncpu := runtime.NumCPU()
 
-	simulate(n, ncpu, 3, r)
-	simulate(n, ncpu, 10, r)
-	simulate(n, ncpu, 25, r)
-	simulate(n, ncpu, 50, r)
-	simulate(n, ncpu, 100, r)
+	if len(os.Args) == 2 && os.Args[1] == "pdist" {
+		{
+			const n = 100000
+			pdist(n, ncpu, 3, r)
+		}
+		{
+			const n = 5000000
+			pdist(n, ncpu, 10, r)
+		}
+	} else {
+		const n = 100000
+		simulate(n, ncpu, 3, r)
+		simulate(n, ncpu, 10, r)
+		simulate(n, ncpu, 25, r)
+		simulate(n, ncpu, 50, r)
+		simulate(n, ncpu, 100, r)
+	}
 }

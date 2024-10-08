4bb8fe401746117947044a3aa67300adf32cd3a3
Q.P.Liu <qpliu@yahoo.com>
2024-10-07 17:51:14 -0700

    Come up with code that calculates the exact result for fiddler 20241004 extra credit general case.

--- riddler/2024/20241004.go
+++ riddler/2024/20241004.go
@@ -116,26 +116,91 @@ func pdist(n, ncpu, nDice int, r *rand.Rand) {
 	}
 }
 
+func pdistBruteForceTrial(nDice, preset, roll int, dist [][6]int) {
+	// make the first die always 0 to cut down the calculations
+	// by a factor of 6
+	// make the second die always set to preset for parallelization
+	rolls := map[int]int{0: 1, 1: 0, 2: 0, 3: 0, 4: 0, 5: 0}
+	rolls[preset]++
+	for _ = range nDice - 2 {
+		rolls[roll%6]++
+		roll /= 6
+	}
+	firstCount := 0
+	numCount := 0
+	for _, count := range rolls {
+		if count > firstCount {
+			firstCount = count
+			numCount = 1
+		} else if count == firstCount {
+			numCount++
+		}
+	}
+	dist[firstCount-1][numCount-1]++
+}
+
+func pdistBruteForce(nDice int) {
+	n := 1
+	for _ = range nDice - 2 {
+		n *= 6
+	}
+	ch := make(chan [][6]int)
+	for i := range 6 {
+		go func(preset int) {
+			dist := make([][6]int, nDice)
+			for roll := range n {
+				pdistBruteForceTrial(nDice, preset, roll, dist)
+			}
+			ch <- dist
+		}(i)
+	}
+	totals := make([][6]int, nDice)
+	for _ = range 6 {
+		dist := <-ch
+		for i := range dist {
+			for j := range dist[i] {
+				totals[i][j] += dist[i][j]
+			}
+		}
+	}
+	for i, tots := range totals {
+		tot := 0
+		isSplit := false
+		for k, count := range tots {
+			if count > 0 {
+				fmt.Printf("%d %d,%d: %d/%d %g\n", nDice, i+1, k+1, count, n*6, float64(count)/float64(n*6))
+				if tot > 0 {
+					isSplit = true
+				}
+				tot += count
+			}
+		}
+		if isSplit {
+			fmt.Printf("%d %d: %d/%d %g\n", nDice, i+1, tot, n*6, float64(tot)/float64(n*6))
+		}
+	}
+}
+
 func main() {
 	const seed = 20241004
 	r := rand.New(rand.NewSource(seed))
 	ncpu := runtime.NumCPU()
 
 	if len(os.Args) == 2 && os.Args[1] == "pdist" {
-		{
-			const n = 100000
-			pdist(n, ncpu, 3, r)
-		}
-		{
-			const n = 5000000
-			pdist(n, ncpu, 10, r)
-		}
+		pdistBruteForce(3)
+		pdistBruteForce(4)
+
+		const n = 5000000
+		pdist(n, ncpu, 10, r)
 	} else {
 		const n = 100000
 		simulate(n, ncpu, 3, r)
+		simulate(n, ncpu, 6, r)
 		simulate(n, ncpu, 10, r)
+		simulate(n, ncpu, 12, r)
 		simulate(n, ncpu, 25, r)
 		simulate(n, ncpu, 50, r)
+		simulate(n, ncpu, 60, r)
 		simulate(n, ncpu, 100, r)
 	}
 }

131a1baea9761beb8ba962946f9bd44a4cc72ebe
Q.P.Liu <qpliu@yahoo.com>
2024-12-12 20:49:06 -0800

    Speed up Advent of Code day 12 by about another 10%.

--- aoc/2024/aoc202412.hs
+++ aoc/2024/aoc202412.hs
@@ -1,8 +1,7 @@
 module AOC202412 where
 
-import Data.Array(Array,array,assocs,bounds,inRange,inRange,(!))
-import Data.Map(Map,alter,elems,empty,insert,keys,member,toList)
-import qualified Data.Map
+import Data.Array(Array,assocs,bounds,inRange,(!))
+import Data.Set(Set,empty,insert,member)
 
 import AOC
 
@@ -24,45 +23,37 @@ aoc = AOC {
     testData2="",
     testResult2="1206",
     aocParse=parse2da,
-    aocResult=result,
+    aocResult=result perimeter,
     aocParse2=parse2da,
-    aocResult2=result2
+    aocResult2=result sides
     }
 
-makeRegions :: Array (Int,Int) Char -> Array (Int,Int) (Int,Int)
-makeRegions grid =
-    array (bounds grid) $ toList $ foldr fill empty $ assocs grid
+measureRegions :: (Array (Int,Int) Char -> Char -> (Int,Int) -> Int)
+               -> Array (Int,Int) Char -> [(Int,Int)]
+measureRegions metric grid = snd $ foldr measure (empty,[]) $ assocs grid
   where
     inBounds = inRange (bounds grid)
-    fill (xy,ch) regions
-      | member xy regions = regions
-      | otherwise = foldr (fillWith xy ch xy) (insert xy xy regions)
-                        $ filter inBounds $ neighbors xy
-    fillWith region ch fromXY xy regions
-      | grid!xy /= ch = regions
-      | member xy regions = regions
-      | otherwise = foldr (fillWith region ch xy) (insert xy region regions)
-                        $ filter inBounds $ filter (/= fromXY) $ neighbors xy
+    measure (xy,ch) (seen,results)
+      | member xy seen = (seen,results)
+      | otherwise = fmap (:results)
+                      $ foldr (fill ch) (insert xy seen,(1,metric grid ch xy))
+                      $ filter inBounds $ neighbors xy
+    fill ch xy (seen,(count,m))
+      | grid!xy /= ch || member xy seen = (seen,(count,m))
+      | otherwise =
+          foldr (fill ch) (insert xy seen,(1+count,m+metric grid ch xy))
+              $ filter inBounds $ neighbors xy
 
-getMetrics :: (Array (Int,Int) (Int,Int) -> (Int,Int) -> (Int,Int) -> Int)
-                -> Array (Int,Int) (Int,Int) -> Map (Int,Int) (Int,Int)
-getMetrics metric regions = foldr collect empty $ assocs regions
-  where
-    collect (xy@(x,y),region) = alter (Just . maybe (1,m) addMetric) region
-      where
-        m = metric regions xy region
-        addMetric (oldCount,oldMetric) = (oldCount+1,oldMetric+m)
-
-perimeter :: Array (Int,Int) (Int,Int) -> (Int,Int) -> (Int,Int) -> Int
-perimeter regions xy region =
+perimeter :: Array (Int,Int) Char -> Char -> (Int,Int) -> Int
+perimeter regions region xy =
     length [() | n <- neighbors xy, not (inBounds n && regions!n == region)]
   where
     inBounds = inRange (bounds regions)
 
 -- Counts south ends of fences to the east, north ends of fences to the west,
 -- east ends of fences to the south, and west ends of fences to the north.
-sides :: Array (Int,Int) (Int,Int) -> (Int,Int) -> (Int,Int) -> Int
-sides regions (x,y) region =
+sides :: Array (Int,Int) Char -> Char -> (Int,Int) -> Int
+sides regions region (x,y) =
     length [() | dxy <- [(1,0),(-1,0),(0,1),(0,-1)], isEnd dxy]
   where
     inRegion (dx,dy) = inRange (bounds regions) (x+dx,y+dy)
@@ -73,8 +64,4 @@ sides regions (x,y) region =
 neighbors :: (Int,Int) -> [(Int,Int)]
 neighbors (x,y) = [(x+1,y),(x-1,y),(x,y+1),(x,y-1)]
 
-result :: Array (Int,Int) Char -> Int
-result = sum . map (uncurry (*)) . elems . getMetrics perimeter . makeRegions
-
-result2 :: Array (Int,Int) Char -> Int
-result2 = sum . map (uncurry (*)) . elems . getMetrics sides . makeRegions
+result metric = sum . map (uncurry (*)) . measureRegions metric

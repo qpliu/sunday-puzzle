6770efedae78ee338ed30afc76924813264d9a06
Q.P.Liu <qpliu@yahoo.com>
2024-12-28 00:21:04 -0800

    Rework Advent of Code 2024 day 24 code some more.

--- aoc/2024/aoc202424.hs
+++ aoc/2024/aoc202424.hs
@@ -2,7 +2,9 @@ module AOC202424 where
 
 import Data.Bits(xor,(.&.),(.|.))
 import Data.List(intercalate,sort)
-import Data.Map(elems,fromList,keys,toList,(!))
+import Data.Map(Map,alter,empty,findWithDefault,fromList,insert,member,(!))
+import Data.Set(Set)
+import qualified Data.Set
 
 import AOC
 
@@ -77,6 +79,31 @@ aoc = AOC {
                 ],
             testResult=Just "2024",
             testResult2=Nothing
+            },
+        AOCTest {
+            testData=unlines [
+                "x00: 0",
+                "x01: 1",
+                "x02: 0",
+                "x03: 1",
+                "x04: 0",
+                "x05: 1",
+                "y00: 0",
+                "y01: 0",
+                "y02: 1",
+                "y03: 1",
+                "y04: 0",
+                "y05: 1",
+                "",
+                "x00 AND y00 -> z05",
+                "x01 AND y01 -> z02",
+                "x02 AND y02 -> z01",
+                "x03 AND y03 -> z03",
+                "x04 AND y04 -> z04",
+                "x05 AND y05 -> z00"
+                ],
+            testResult=Nothing,
+            testResult2=Nothing
             }
         ],
     aocCode=Code {
@@ -89,131 +116,142 @@ aoc = AOC {
         }
     }
 
-data Expr = C Int
-          | I String
-          | XOR Expr Expr String
-          | OR  Expr Expr String
-          | AND Expr Expr String
+data Op = IN Int
+        | XOR String String String
+        | OR  String String String
+        | AND String String String
     deriving (Eq,Ord,Show)
 
+parse :: String -> [(String,Op)]
 parse = map p . filter (not . null) . map words . lines
   where
-    p [a,b] = (I (init a),C (read b))
-    p [a,"XOR",b,"->",c] = (I c,XOR (I a) (I b) c)
-    p [a,"OR", b,"->",c] = (I c,OR  (I a) (I b) c)
-    p [a,"AND",b,"->",c] = (I c,AND (I a) (I b) c)
+    p [a,b] = (init a,IN $ read b)
+    p [a,"XOR",b,"->",c] = (c,XOR a b c)
+    p [a,"OR", b,"->",c] = (c,OR  a b c)
+    p [a,"AND",b,"->",c] = (c,AND a b c)
 
-result input = sum $ zipWith (*) (map (table!) (zs table)) [2^n | n <- [0..]]
+zs :: [(String,Op)] -> [String]
+zs = sort . filter ((== 'z') . head) . map fst
+
+result input = sum $ zipWith (*) (map (table!) $ zs input) [2^n | n <- [0..]]
   where
-    table = fromList $ map f input
+    table = fromList $ map (fmap op) input
+    op (IN n) = n
+    op (XOR a b _) = table!a `xor` table!b
+    op (OR  a b _) = table!a .|.   table!b
+    op (AND a b _) = table!a .&.   table!b
 
-    f (k,C v) = (k,v)
-    f (k,XOR a b _) = (k,(table!a) `xor` (table!b))
-    f (k,OR  a b _) = (k,(table!a) .|.   (table!b))
-    f (k,AND a b _) = (k,(table!a) .&.   (table!b))
-
-zs table = sort $ filter isZ $ keys table
+tabulate :: [(String,Op)]
+         -> (Map String [Op],Map String [Op],Map String [Op],
+             Map String Op,Set String)
+tabulate = foldr collect (empty,empty,empty,empty,Data.Set.empty)
   where
-    isZ (I ('z':_)) = True
-    isZ _ = False
+    collect (_,op@(XOR a b c)) (xors,ors,ands,outs,ins) =
+        (add op a b xors,ors,ands,insert c op outs,ins)
+    collect (_,op@(OR  a b c)) (xors,ors,ands,outs,ins) =
+        (xors,add op a b ors,ands,insert c op outs,ins)
+    collect (_,op@(AND a b c)) (xors,ors,ands,outs,ins) =
+        (xors,ors,add op a b ands,insert c op outs,ins)
+    collect (v,_) (xors,ors,ands,outs,ins) =
+        (xors,ors,ands,outs,Data.Set.insert v ins)
+    add op a b =
+        alter (Just . maybe [op] (op:)) a . alter (Just . maybe [op] (op:)) b
 
-prev (I ['z',c,'0']) = (I ['z',pred c,'9'])
-prev (I ['z',c,d]) = (I ['z',c,pred d])
+x00 = "x00"
+y00 = "y00"
+z00 = "z00"
+next [c,tens,'9'] = [c,succ tens,'0']
+next [c,tens,ones] = [c,tens,succ ones]
 
-x (I ('z':i)) = I ('x':i)
-x _ = C 2
-y (I ('z':i)) = I ('y':i)
-y _ = C 3
-toz (I ('x':i)) = I ('z':i)
-toz _ = C 4
-z00 = I "z00"
-
-makeTable swaps input = table
+swapList :: (Map String [Op],Map String [Op],Map String [Op],
+             Map String Op,Set String)
+         -> [String]
+swapList (xors,ors,ands,outs,ins) = checkAdd00
   where
-    table = fromList $ map (f . doSwaps) $ input
-    doSwaps (I a,expr) = (I $ swap a swaps,expr)
-    doSwaps expr = expr
+    addSwap :: String -> String -> Map String String -> Map String String
+    addSwap a b swaps = insert a b $ insert b a swaps
+    swap :: String -> Map String String -> String
+    swap a swaps = findWithDefault a a swaps
 
-    swap a [] = a
-    swap a ([b,c]:rest)
-      | a == b = c
-      | a == c = b
-      | otherwise = swap a rest
-
-    f (k,C _) = (k,k)
-    f (k,XOR a b i) = kv k XOR a b i
-    f (k,OR  a b i) = kv k OR  a b i
-    f (k,AND a b i) = kv k AND a b i
-
-    kv k op a b i = (k,op (min aexpr bexpr) (max aexpr bexpr) i)
+    checkZ :: String -> String -> Map String String -> Bool
+    checkZ z out swaps = checkZout (outs!z)
       where
-        aexpr = table!a
-        bexpr = table!b
+        checkZout (XOR _ _ outz) = out == swap outz swaps
+        checkZout _ = False
 
-isXOR z (XOR a b _) = a == x z && b == y z
-isXOR _ _ = False
-
-isAdd zmax z expr@(XOR a b _)
-  | z == z00 = isXOR z expr
-  | otherwise = isXOR z a && isCarry (prev z) b
-isAdd zmax z expr
-  | z == zmax = isCarry (prev z) expr
-  | otherwise = False
-
--- with zNN, just checking that [xNN AND yNN] exists somewhere in
--- the expression is enough for my input
-isCarry z (AND a b _)
-  | a == x z && b == y z = True
-  | otherwise = isCarry z a || isCarry z b
-isCarry z (XOR a b _) = isCarry z a || isCarry z b
-isCarry z (OR a b _) = isCarry z a || isCarry z b
-isCarry z _ = False
-
-findSwap input swaps = check $ zs table
-  where
-    table = makeTable swaps input
-    zmax = maximum $ zs table
-
-    xorXYs = fromList [(z,expr) | expr <- elems table, z <- zForXORxy expr]
-    zForXORxy (XOR x1 y1 _)
-      | x1 == x (toz x1) && y1 == y (toz x1) = [toz x1]
-      | otherwise = []
-    zForXORxy _ = []
-
-    addXYs = fromList [(z,expr) | expr <- elems table, z <- zForAddXY expr]
-    zForAddXY (XOR (XOR x1 y1 _) _ _)
-      | x1 == x (toz x1) && y1 == y (toz x1) = [toz x1]
-      | otherwise = []
-    zForAddXY (XOR _ (XOR x1 y1 _) _)
-      | x1 == x (toz x1) && y1 == y (toz x1) = [toz x1]
-      | otherwise = []
-    zForAddXY _ = []
-
-    check [] = []
-    check (z@(I zi):rest)
-      | z == z00 && table!z == xorXYs!z = check rest
-      | z == zmax = checkCarry (prev z) (table!z)
-      | otherwise = checkAdd (table!z)
+    checkAdd00 :: [String]
+    checkAdd00
+      | xorX00 /= xorY00 = error (show (xorX00,xorY00))
+      | checkZ z00 xorX00 empty = checkCarry00 empty
+      | otherwise = z00 : xorX00 : checkCarry00 (addSwap z00 xorX00 empty)
       where
-        checkAdd (XOR a b _)
-          | a == xorXY = checkCarry (prev z) b
-          | b == xorXY = checkCarry (prev z) a
-          | isCarry (prev z) a = makeSwap b
-          | isCarry (prev z) b = makeSwap a
-          | otherwise = error (show z)
-          where
-            xorXY@(XOR _ _ xorXYi) = xorXYs!z
-            makeSwap (XOR _ _ i) = [i,xorXYi]
-            makeSwap (OR _ _ i) = [i,xorXYi]
-            makeSwap (AND _ _ i) = [i,xorXYi]
-        checkAdd _ = zi : [ai | (XOR _ _ ai) <- [addXYs!z]]
+        (XOR _ _ xorX00:_) = xors!x00
+        (XOR _ _ xorY00:_) = xors!y00
 
-        checkCarry zc _ = check rest -- not needed for my input
+    checkCarry00 :: Map String String -> [String]
+    checkCarry00 swaps
+      | andX00 /= andY00 = error (show (andX00,andY00))
+      | otherwise = checkAdd (next x00) (next y00) andX00 (next z00) swaps
+      where
+        (AND _ _ andX00:_) = ands!x00
+        (AND _ _ andY00:_) = ands!y00
 
-result2 input = format $ swaps []
-  where
-    swaps swapList
-      | null swap = []
-      | otherwise = swap : swaps (swap:swapList)
-      where swap = findSwap input swapList
-    format = intercalate "," . sort . concat
+    checkAdd :: String -> String -> String -> String -> Map String String
+             -> [String]
+    checkAdd x y carry z swaps
+      | not (Data.Set.member x ins) && z == carry = []
+      | not (Data.Set.member x ins) = [carry,z]
+        -- only (x XOR y) XOR carry in my input
+        -- no cases of (x XOR carry) XOR y in my input
+        -- no cases of (y XOR carry) XOR x in my input
+      | swap xorX swaps /= swap xorY swaps = undefined
+      | not (member (swap xorX swaps) xors)
+              && xxory2 == swap carry swaps =
+          carry2:xorX:checkAdd x y carry z (addSwap carry2 xorX swaps)
+      | not (member (swap xorX swaps) xors)
+              && carry2 == swap carry swaps =
+          xxory2:xorX:checkAdd x y carry z (addSwap xxory2 xorX swaps)
+      | not (member (swap carry swaps) xors)
+              && xxory1 == swap carry swaps =
+          carry1:carry:checkAdd x y carry z (addSwap carry1 carry swaps)
+      | not (member (swap carry swaps) xors)
+              && carry1 == swap carry swaps =
+          xxory1:carry:checkAdd x y carry z (addSwap xxory1 carry swaps)
+      | swap xorX swaps == swap xorY swaps
+              && swap xorXorX swaps == swap xorCarry swaps
+              && checkZ z xorXorX swaps =
+          checkCarry x y carry z swaps
+      | swap xorX swaps == swap xorY swaps
+              && swap xorXorX swaps == swap xorCarry swaps =
+          z:xorXorX:checkCarry x y carry z (addSwap z xorXorX swaps)
+      | otherwise = undefined
+      where
+        (XOR _ _ xorX:_) = xors!x
+        (XOR _ _ xorY:_) = xors!y
+        (XOR xxory1 carry1 xorXorX:_) = xors!swap xorX swaps
+        (XOR xxory2 carry2 xorCarry:_) = xors!swap carry swaps
+
+    checkCarry :: String -> String -> String -> String -> Map String String
+               -> [String]
+    checkCarry x y carry z swaps
+        -- only ((x XOR y) AND carry) OR (x AND y) in my input
+        -- no cases of ((x XOR carry) AND y) OR (x AND carry) in my input
+        -- no cases of ((y XOR carry) AND x) OR (y AND carry) in my input
+      | swap xorX swaps /= swap xorY swaps = undefined
+      | swap andX swaps /= swap andY swaps = undefined
+      | swap andXorX swaps == swap andCarry swaps
+              && swap andX swaps == swap andY swaps
+              && swap orAndXorX swaps == swap orAndX swaps =
+          checkAdd (next x) (next y) orAndX (next z) swaps
+      | otherwise = undefined
+      where
+        (XOR _ _ xorX:_) = xors!x
+        (XOR _ _ xorY:_) = xors!y
+        (AND _ _ andXorX:_) = ands!swap xorX swaps
+        (AND _ _ andCarry:_) = ands!swap carry swaps
+        (AND _ _ andX:_) = ands!x
+        (AND _ _ andY:_) = ands!y
+        (OR _ _ orAndXorX:_) = ors!swap andXorX swaps
+        (OR _ _ orAndX:_) = ors!swap andX swaps
+
+result2 = sort . swapList . tabulate

160b3ce15449e266eaec443b0426d8841b18e635
Q.P.Liu <qpliu@yahoo.com>
2025-12-09 12:09:08 -0800

    Speed up Advent of Code day 9 by adding heuristics that could fail with perverse inputs.  For my input on my computer, part 1 goes from over 200ms to about 8ms, and part 2 goes from about 4.7sec to about 2.7sec, which is more than 10 times faster than my initial solution that took almost 30sec.  Code is still ugly.

--- aoc/2025/aoc202509.hs
+++ aoc/2025/aoc202509.hs
@@ -34,24 +34,48 @@ aoc = AOC {
         }
     }
 
-parse = toPairs . parseInts
+parse = addExtremes . toPairs . parseInts
   where
     toPairs (x:y:rest) = (x,y):toPairs rest
     toPairs _ = []
 
-result input = maximum [(1 + abs (x1-x2))*(1 + abs (y1-y2)) | p1@(x1,y1) <- input, p2@(x2,y2) <- input, p1 < p2]
+addExtremes pairs = (minimum $ map fst pairs,maximum $ map fst pairs,
+                     minimum $ map snd pairs,maximum $ map snd pairs,
+                     pairs)
 
-result2 input =
-    maximum $ [(1 + abs (xbig!xc2-xbig!xc1))*(1 + abs (ybig!yc2-ybig!yc1))
+area (x1,y1) (x2,y2) = (1 + abs (x1-x2))*(1 + abs (y1-y2))
+
+result (xmin,xmax,ymin,ymax,pairs) =
+    maximum [area p1 p2 | (_,p1) <- candidates, (_,p2) <- candidates, p1 < p2]
+  where
+    withDist2 x0 y0 (x,y) = ((x-x0)^2+(y-y0)^2,(x,y))
+    candidates = take 10 (sort (map (withDist2 xmin ymin) pairs))
+              ++ take 10 (sort (map (withDist2 xmax ymin) pairs))
+              ++ take 10 (sort (map (withDist2 xmin ymax) pairs))
+              ++ take 10 (sort (map (withDist2 xmax ymax) pairs))
+
+possibleCornerPair (upperLeft,ulConvex) (lowerRight,lrConvex) True =
+    (upperLeft == 'F') == ulConvex && (lowerRight == 'J') == lrConvex
+possibleCornerPair (lowerLeft,llConvex) (upperRight,urConvex) False =
+    (lowerLeft == 'L') == llConvex && (upperRight == '7') == urConvex
+
+result2 (xmin,xmax,ymin,ymax,input) =
+    maximum $ [a
                | (xc1,yc1) <- cpoints, (xc2,yc2) <- cpoints,
                  xc1 < xc2, yc1 /= yc2,
+                 possibleCornerPair (cgrid!(xc1,yc1)) (cgrid!(xc2,yc2))
+                                    (yc2 > yc1),
+                 a <- [area (xbig!xc1,ybig!yc1) (xbig!xc2,ybig!yc2)],
+                 a > heuristicMinArea,
                  and [member (xc,yc) cgrid
                       | xc <- [xc1..xc2], yc <- [yc1,yc2]],
                  and [member (xc,yc) cgrid
                       | xc <- [xc1,xc2], yc <- [min yc1 yc2 .. max yc1 yc2]],
-                 and [findWithDefault '.' (xc,yc) cgrid == '#'
-                      | xc <- [xc1+1..xc2-1], yc <- [min yc1 yc2 + 1 .. max yc1 yc2 - 1]]]
+                 and [findWithDefault ('.',True) (xc,yc) cgrid == ('#',False)
+                      | xc <- [xc1+1..xc2-1],
+                        yc <- [min yc1 yc2 + 1 .. max yc1 yc2 - 1]]]
   where
+    heuristicMinArea = area (xmin,ymin) (xmax,ymax) `div` 6
     xcompact = fromList $ zip (sort $ nub $ map fst input) [0..]
     ycompact = fromList $ zip (sort $ nub $ map snd input) [0..]
     xbig = fromList $ map swap $ toList xcompact
@@ -60,7 +84,7 @@ result2 input =
     ycmax = maximum ycompact
     cpoints = map (\ (x,y) -> (xcompact!x,ycompact!y)) input
     loop = fromList $ makeLoop (last cpoints:cpoints ++ take 1 cpoints)
-    cgrid = union loop $ fromList $ fillLoop 0 0 False
+    cgrid = fromList $ fillLoop 0 0 False
     makeLoop ((x1,y1):rest@((x2,y2):(x3,y3):_))
       | x1 < x2 && y2 < y3 = ((x2,y2),'7') : [((x2,y),'|') | y <- [y2+1 .. y3-1]] ++ makeLoop rest
       | x1 < x2 && y2 > y3 = ((x2,y2),'J') : [((x2,y),'|') | y <- [y3+1 .. y2-1]] ++ makeLoop rest
@@ -75,11 +99,15 @@ result2 input =
       | y > ycmax = if inLoop then error "y > ycmax" else []
       | x > xcmax = if inLoop then error ("x > xcmax, y=" ++ show y) else fillLoop 0 (y+1) False
       | not (member (x,y) loop) =
-          if inLoop then ((x,y),'#'):fillLoop (x+1) y inLoop
+          if inLoop then ((x,y),('#',False)):fillLoop (x+1) y inLoop
                     else fillLoop (x+1) y inLoop
-      | loop!(x,y) == '|' = fillLoop (x+1) y (not inLoop)
-      | loop!(x,y) == 'F' = fillLoop (x+1) y (not inLoop)
-      | loop!(x,y) == 'L' = fillLoop (x+1) y inLoop
-      | loop!(x,y) == 'J' = fillLoop (x+1) y inLoop
-      | loop!(x,y) == '7' = fillLoop (x+1) y (not inLoop)
-      | otherwise = fillLoop (x+1) y inLoop
+      | loop!(x,y) == '|' = ((x,y),('|',False)):fillLoop (x+1) y (not inLoop)
+      | loop!(x,y) == 'F' =
+          ((x,y),('F',not inLoop)):fillLoop (x+1) y (not inLoop)
+      | loop!(x,y) == 'L' =
+          ((x,y),('L',not inLoop)):fillLoop (x+1) y inLoop
+      | loop!(x,y) == 'J' =
+          ((x,y),('J',not inLoop)):fillLoop (x+1) y inLoop
+      | loop!(x,y) == '7' =
+          ((x,y),('7',inLoop)):fillLoop (x+1) y (not inLoop)
+      | loop!(x,y) == '-' = ((x,y),('-',False)):fillLoop (x+1) y inLoop

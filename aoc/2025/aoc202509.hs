816666b9d34eeeceb8da6635ff711668d3880b23
Q.P.Liu <qpliu@yahoo.com>
2025-12-11 18:15:13 -0800

    Speed up Advent of Code 2025 day 9 part 2 with heuristic based on the shape of the input, so part 2 now takes about 22ms on my computer with my input.

--- aoc/2025/aoc202509.hs
+++ aoc/2025/aoc202509.hs
@@ -1,8 +1,7 @@
 module AOC202509 where
 
 import Data.List(nub,sort)
-import Data.Map(fromList,insert,findWithDefault,member,toList,union,(!))
-import Data.Tuple(swap)
+import Data.Map(empty,fromList,insert,findWithDefault,(!))
 
 import AOC
 
@@ -46,68 +45,74 @@ addExtremes pairs = (minimum $ map fst pairs,maximum $ map fst pairs,
 area (x1,y1) (x2,y2) = (1 + abs (x1-x2))*(1 + abs (y1-y2))
 
 result (xmin,xmax,ymin,ymax,pairs) =
-    maximum [area p1 p2 | (_,p1) <- candidates, (_,p2) <- candidates, p1 < p2]
+    maximum [area p1 p2 | (_,p1) <- candidates1, (_,p2) <- candidates2]
   where
     withDist2 x0 y0 (x,y) = ((x-x0)^2+(y-y0)^2,(x,y))
-    candidates = take 10 (sort (map (withDist2 xmin ymin) pairs))
-              ++ take 10 (sort (map (withDist2 xmax ymin) pairs))
-              ++ take 10 (sort (map (withDist2 xmin ymax) pairs))
-              ++ take 10 (sort (map (withDist2 xmax ymax) pairs))
-
-possibleCornerPair (upperLeft,ulConvex) (lowerRight,lrConvex) True =
-    (upperLeft == 'F') == ulConvex && (lowerRight == 'J') == lrConvex
-possibleCornerPair (lowerLeft,llConvex) (upperRight,urConvex) False =
-    (lowerLeft == 'L') == llConvex && (upperRight == '7') == urConvex
-
-result2 (xmin,xmax,ymin,ymax,input) =
-    maximum $ [a
-               | (xc1,yc1) <- cpoints, (xc2,yc2) <- cpoints,
-                 xc1 < xc2, yc1 /= yc2,
-                 possibleCornerPair (cgrid!(xc1,yc1)) (cgrid!(xc2,yc2))
-                                    (yc2 > yc1),
-                 a <- [area (xbig!xc1,ybig!yc1) (xbig!xc2,ybig!yc2)],
-                 a > heuristicMinArea,
-                 and [member (xc,yc) cgrid
-                      | xc <- [xc1..xc2], yc <- [yc1,yc2]],
-                 and [member (xc,yc) cgrid
-                      | xc <- [xc1,xc2], yc <- [min yc1 yc2 .. max yc1 yc2]],
-                 and [findWithDefault ('.',True) (xc,yc) cgrid == ('#',False)
-                      | xc <- [xc1+1..xc2-1],
-                        yc <- [min yc1 yc2 + 1 .. max yc1 yc2 - 1]]]
+    -- heuristic: expect the largest rectangle to have corners that are
+    -- one of the closest to opposite corners of the bounding rectangle
+    candidates1 = take 5 (sort (map (withDist2 xmin ymin) pairs))
+               ++ take 5 (sort (map (withDist2 xmin ymax) pairs))
+    candidates2 = take 5 (sort (map (withDist2 xmax ymin) pairs))
+               ++ take 5 (sort (map (withDist2 xmax ymax) pairs))
+ 
+result2 (xmin,xmax,ymin,ymax,pairs) =
+    -- heuristic based on shape of the input: the largest rectangle will
+    -- have a corner that is one of the 4 corners connected by one of the
+    -- two longest segments to one of its neighbors
+    scanRectangles 1 (take 4 corners) corners
   where
-    heuristicMinArea = area (xmin,ymin) (xmax,ymax) `div` 6
-    xcompact = fromList $ zip (sort $ nub $ map fst input) [0..]
-    ycompact = fromList $ zip (sort $ nub $ map snd input) [0..]
-    xbig = fromList $ map swap $ toList xcompact
-    ybig = fromList $ map swap $ toList ycompact
-    xcmax = maximum xcompact
-    ycmax = maximum ycompact
-    cpoints = map (\ (x,y) -> (xcompact!x,ycompact!y)) input
-    loop = fromList $ makeLoop (last cpoints:cpoints ++ take 1 cpoints)
-    cgrid = fromList $ fillLoop 0 0 False
-    makeLoop ((x1,y1):rest@((x2,y2):(x3,y3):_))
-      | x1 < x2 && y2 < y3 = ((x2,y2),'7') : [((x2,y),'|') | y <- [y2+1 .. y3-1]] ++ makeLoop rest
-      | x1 < x2 && y2 > y3 = ((x2,y2),'J') : [((x2,y),'|') | y <- [y3+1 .. y2-1]] ++ makeLoop rest
-      | x1 > x2 && y2 < y3 = ((x2,y2),'F') : [((x2,y),'|') | y <- [y2+1 .. y3-1]] ++ makeLoop rest
-      | x1 > x2 && y2 > y3 = ((x2,y2),'L') : [((x2,y),'|') | y <- [y3+1 .. y2-1]] ++ makeLoop rest
-      | y1 < y2 && x2 < x3 = ((x2,y2),'L') : [((x,y2),'-') | x <- [x2+1 .. x3-1]] ++ makeLoop rest
-      | y1 < y2 && x2 > x3 = ((x2,y2),'J') : [((x,y2),'-') | x <- [x3+1 .. x2-1]] ++ makeLoop rest
-      | y1 > y2 && x2 < x3 = ((x2,y2),'F') : [((x,y2),'-') | x <- [x2+1 .. x3-1]] ++ makeLoop rest
-      | y1 > y2 && x2 > x3 = ((x2,y2),'7') : [((x,y2),'-') | x <- [x3+1 .. x2-1]] ++ makeLoop rest
-    makeLoop _ = []
-    fillLoop x y inLoop
-      | y > ycmax = if inLoop then error "y > ycmax" else []
-      | x > xcmax = if inLoop then error ("x > xcmax, y=" ++ show y) else fillLoop 0 (y+1) False
-      | not (member (x,y) loop) =
-          if inLoop then ((x,y),('#',False)):fillLoop (x+1) y inLoop
-                    else fillLoop (x+1) y inLoop
-      | loop!(x,y) == '|' = ((x,y),('|',False)):fillLoop (x+1) y (not inLoop)
-      | loop!(x,y) == 'F' =
-          ((x,y),('F',not inLoop)):fillLoop (x+1) y (not inLoop)
-      | loop!(x,y) == 'L' =
-          ((x,y),('L',not inLoop)):fillLoop (x+1) y inLoop
-      | loop!(x,y) == 'J' =
-          ((x,y),('J',not inLoop)):fillLoop (x+1) y inLoop
-      | loop!(x,y) == '7' =
-          ((x,y),('7',inLoop)):fillLoop (x+1) y (not inLoop)
-      | loop!(x,y) == '-' = ((x,y),('-',False)):fillLoop (x+1) y inLoop
+    xcompact = fromList $ zip (sort $ nub $ map fst pairs) [0..]
+    ycompact = fromList $ zip (sort $ nub $ map snd pairs) [0..]
+
+    -- sort by negative of the length of longest segment to neighbor
+    -- so that the longest ones come first
+    corners = sort $ makeCorners (last pairs:pairs ++ [head pairs])
+    makeCorners ((x1,y1):rest@((x2,y2):(x3,y3):_)) =
+        (- maximum [abs (x1-x2),abs (y1-y2),abs (x2-x3),abs (y2-y3)],
+         (xcompact!x2,ycompact!y2),(x2,y2)) : makeCorners rest
+    makeCorners _ = []
+
+    scanRectangles curMaxArea [] _ = curMaxArea
+    scanRectangles curMaxArea (_:corners1) [] =
+        scanRectangles curMaxArea corners1 corners
+    scanRectangles curMaxArea corners1@(c1@(_,_,xy1):_)
+                              (c2@(_,_,xy2):corners2)
+      | curArea <= curMaxArea || hsegThrough c1 c2 || vsegThrough c1 c2 =
+          scanRectangles curMaxArea corners1 corners2
+      | otherwise =
+          scanRectangles curArea corners1 corners2
+      where curArea = area xy1 xy2
+    
+    hsegs = makeHsegs empty (last pairs:pairs)
+      where
+        makeHsegs segs ((x1,y1):rest@((x2,y2):_))
+          | x1 == x2 = makeHsegs segs rest
+          | otherwise =
+              makeHsegs (insert (ycompact!y1)
+                                ((xcompact!(min x1 x2),xcompact!(max x1 x2)) :
+                                 findWithDefault [] (ycompact!y1) segs)
+                                segs)
+                        rest
+        makeHsegs segs _ = segs
+    hsegThrough (_,(xc1,yc1),_) (_,(xc2,yc2),_) =
+        not $ null [() | yc <- [min yc1 yc2 + 1 .. max yc1 yc2 - 1],
+                         not $ null [() | (xseg1,xseg2) <- hsegs!yc,
+                                          min xc1 xc2 < xseg2,
+                                          max xc1 xc2 > xseg1]]
+
+    vsegs = makeVsegs empty (last pairs:pairs)
+      where
+        makeVsegs segs ((x1,y1):rest@((x2,y2):_))
+          | y1 == y2 = makeVsegs segs rest
+          | otherwise =
+              makeVsegs (insert (xcompact!x1)
+                                ((ycompact!(min y1 y2),ycompact!(max y1 y2)) :
+                                 findWithDefault [] (xcompact!x1) segs)
+                                segs)
+                        rest
+        makeVsegs segs _ = segs
+    vsegThrough (_,(xc1,yc1),_) (_,(xc2,yc2),_) =
+        not $ null [() | xc <- [min xc1 xc2 + 1 .. max xc1 xc2 - 1],
+                         not $ null [() | (yseg1,yseg2) <- vsegs!xc,
+                                          min yc1 yc2 < yseg2,
+                                          max yc1 yc2 > yseg1]]

aa45a24b2c4d347fc87bd5afee2c6e7fc612dd47
Q.P.Liu <qpliu@yahoo.com>
2025-12-13 17:22:39 -0800

    Speed up Advent of Code 2025 day 8 with more efficient of generation of pairs plus heuristic of eliminating pairs more distant than some value.  The biggest gain was from changing the dist2 function, let dx=x1-x2 in dx*dx is much faster than (x1-x2)^2.  Part 1 goes from about 1.4s to about 650s, and part 2 goes from about 1.5s to about 710ms.  Also explored using mutable vectors, but that was slightly slower.  Every other 2025 solution runs in under 500ms, and, other than day 4 part 2, runs in under 80ms, on my computer for my input.

--- aoc/2025/aoc202508.hs
+++ aoc/2025/aoc202508.hs
@@ -4,6 +4,14 @@ import Data.List(sort)
 import Data.Map(delete,empty,member,insert,toList,(!))
 import qualified Data.Map
 
+{-
+import Control.Monad.ST(runST)
+import Data.List(sort)
+import Data.Vector.Unboxed((!))
+import qualified Data.Vector.Unboxed as V
+import qualified Data.Vector.Unboxed.Mutable as VM
+-}
+
 import AOC
 
 aoc = AOC {
@@ -46,20 +54,31 @@ aoc = AOC {
         }
     }
 
-parse = toCoords . parseInts
+parse = toInput . toCoords . parseInts
   where
     toCoords (x:y:z:rest) = (x,y,z) : toCoords rest
     toCoords _ = []
+    toInput coords = (length coords,sort (toPairsWithDist2 coords))
+    toPairsWithDist2 (a:rest) =
+        -- 200000000 is a heuristic to speed up the calculation
+        -- by throwing away the most distant pairs.  Making it
+        -- larger makes the calculation slower.  Making it too
+        -- small makes the calculation give the wrong answer.
+        [(d2,a,b) | b <- rest, d2 <- [dist2 a b], d2 < 200000000]
+            ++ toPairsWithDist2 rest
+    toPairsWithDist2 _ = []
+    toPairWithDist2 a b = (dist2 a b,a,b)
 
-dist2 (x1,y1,z1) (x2,y2,z2) = (x1-x2)^2 + (y1-y2)^2 + (z1-z2)^2
+dist2 (x1,y1,z1) (x2,y2,z2) = dx*dx + dy*dy + dz*dz
+  where
+    dx = x1-x2
+    dy = y1-y2
+    dz = z1-z2
 
-result npairs input =
+result npairs (_,input) =
     product $ take 3 $ reverse $ sort $ map snd $ toList sizes
   where
-    (sizes,circuits) =
-        foldr collect (empty,empty)
-            $ take npairs
-            $ sort [(dist2 a b,a,b) | a <- input, b <- input, a < b]
+    (sizes,circuits) = foldr collect (empty,empty) $ take npairs input
     collect (_,a,b) (sizes,circuits)
       | havea && haveb && ak == bk = (sizes,circuits)
       | havea && haveb =
@@ -74,29 +93,24 @@ result npairs input =
         ak = circuits!a
         bk = circuits!b
 
-result2 input = 
-    collect 0 empty empty $ sort [(dist2 a b,a,b) | a <- input, b <- input,
-                                                    a < b]
+result2 (nboxes,input) = collect empty empty input
   where
-    nboxes = length input
-    collect last2 sizes circuits ((_,a@(ax,_,_),b@(bx,_,_)):rest)
-      | not (null sizes) && maximum sizes == nboxes = last2
-      | havea && haveb && ak == bk = collect (ax*bx) sizes circuits rest
+    collect sizes circuits ((_,a@(ax,_,_),b@(bx,_,_)):rest)
+      | havea && haveb && ak == bk = collect sizes circuits rest
+      | havea && haveb && asize + bsize == nboxes = ax*bx
       | havea && haveb =
-            collect (ax*bx)
-                    (insert ak (sizes!ak + sizes!bk) (delete bk sizes))
+            collect (insert ak (asize+bsize) (delete bk sizes))
                     (Data.Map.map (\ k -> if k == bk then ak else k) circuits)
                     rest
-      | havea = collect (ax*bx)
-                        (insert ak (sizes!ak + 1) sizes)
+      | havea && asize+1 == nboxes = ax*bx
+      | havea = collect (insert ak (asize + 1) sizes)
                         (insert b ak circuits)
                         rest
-      | haveb = collect (ax*bx)
-                        (insert bk (sizes!bk + 1) sizes)
+      | haveb && bsize+1 == nboxes = ax*bx
+      | haveb = collect (insert bk (bsize + 1) sizes)
                         (insert a bk circuits)
                         rest
-      | otherwise = collect (ax*bx)
-                            (insert a 2 sizes)
+      | otherwise = collect (insert a 2 sizes)
                             (insert b a (insert a a circuits))
                             rest
       where
@@ -104,3 +118,87 @@ result2 input =
         haveb = member b circuits
         ak = circuits!a
         bk = circuits!b
+        asize = sizes!ak
+        bsize = sizes!bk
+
+{-
+-- using mutable vectors is slightly slower above approach
+
+parse :: String -> (V.Vector Int,[(Int,Int)])
+parse = toInput . zip [0..] . toCoords . parseInts
+  where
+    toCoords (x:y:z:rest) = (x,y,z) : toCoords rest
+    toCoords _ = []
+    toInput coords =
+        (V.fromList (map getX coords),map snd (sort (toPairs coords)))
+    toPairs ((ai,axyz):rest) =
+        -- 200000000 is a heuristic to speed up the calculation
+        -- by throwing away the most distant pairs.  Making it
+        -- larger makes the calculation slower.  Making it too
+        -- small makes the calculation give the wrong answer.
+        [(d2,(ai,bi)) | (bi,bxyz) <- rest,
+                        d2 <- [dist2 axyz bxyz],d2 < 200000000]
+            ++ toPairs rest
+    toPairs _ = []
+    getX (_,(x,_,_)) = x
+
+dist2 (x1,y1,z1) (x2,y2,z2) = dx*dx + dy*dy + dz*dz
+  where
+    dx = x1-x2
+    dy = y1-y2
+    dz = z1-z2
+
+result :: Int -> (V.Vector Int,[(Int,Int)]) -> Int
+result npairs (xs,pairs) = runST $ do
+    sizes <- VM.replicate (V.length xs) 0
+    circuits <- V.thaw $ V.generate (V.length xs) id
+    mapM_ (connect sizes circuits) (take npairs pairs)
+    fmap (product . take 3 . reverse . sort . V.toList) (V.freeze sizes)
+
+connect sizes circuits (a,b) = do
+    ac <- VM.read circuits a
+    bc <- VM.read circuits b
+    if ac == bc
+      then return 0
+      else do
+        as <- VM.read sizes ac
+        bs <- VM.read sizes bc
+        if as > 0 && bs > 0 then do
+            VM.write sizes ac (as+bs)
+            VM.write sizes bc 0
+            mapM_ (moveConnection circuits bc ac) [0 .. VM.length circuits - 1]
+            return (as+bs)
+        else if as > 0 then do
+            VM.write sizes ac (as+1)
+            VM.write circuits b ac
+            return (as+1)
+        else if bs > 0 then do
+            VM.write sizes bc (bs+1)
+            VM.write circuits a bc
+            return (bs+1)
+        else do
+            VM.write sizes ac 2
+            VM.write circuits bc ac
+            return 2
+  where
+    moveConnection circuits bc ac i = do
+      ic <- VM.read circuits i
+      if ic == bc
+        then VM.write circuits i ac
+        else return ()
+
+result2 :: (V.Vector Int,[(Int,Int)]) -> Int
+result2 (xs,pairs) = runST $ do
+    sizes <- VM.replicate (V.length xs) 0
+    circuits <- V.thaw $ V.generate (V.length xs) id
+    makeConnections sizes circuits
+  where
+    nboxes = V.length xs
+    makeConnections sizes circuits = conn pairs
+      where
+        conn ((a,b):rest) = do
+            size <- connect sizes circuits (a,b)
+            if size == nboxes
+              then return (xs!a * xs!b)
+              else conn rest
+-}

007feba7a9dd6987b1a26a7e2e62ff135e38df09
Q.P.Liu <qpliu@yahoo.com>
2026-07-17 23:11:59 -0700

    Get an answer for fiddler 20260717 extra credit.

--- riddler/2026/20260717.go
+++ riddler/2026/20260717.go
@@ -110,6 +110,80 @@ func ecsimulate(niter, nbucket int, r *rand.Rand) {
 	fmt.Printf("]]\n%f,%f,%f\n", maxQ, maxS, maxP)
 }
 
+func semisopponent(remaining bool, r *rand.Rand) float64 {
+	if remaining {
+		return 1 - max(r.Float64(), r.Float64())
+	} else {
+		return (1 - max(r.Float64(), r.Float64())) * r.Float64()
+	}
+}
+
+func semisdist(niter, nbucket int, remaining bool, r *rand.Rand) {
+	ch := make(chan []int)
+	for range runtime.NumCPU() {
+		go func(r *rand.Rand) {
+			dist := make([]int, nbucket)
+			for range niter {
+				dist[int(float64(nbucket)*semisopponent(remaining, r))]++
+			}
+			ch <- dist
+		}(rand.New(rand.NewSource(int64(r.Uint64()))))
+	}
+	dist := make([]int, nbucket)
+	total := 0
+	for range runtime.NumCPU() {
+		for i, count := range <-ch {
+			dist[i] += count
+			total += count
+		}
+	}
+	sep := "line(["
+	for i, count := range dist {
+		fmt.Printf("%s(%f,%f)", sep, float64(i)/float64(nbucket), float64(count)/float64(total)*float64(nbucket))
+		sep = ","
+	}
+	fmt.Printf("])\n")
+}
+
+func finalsopponent(r *rand.Rand) float64 {
+	e1 := 1 - max(r.Float64(), r.Float64())
+	e2 := 1 - max(r.Float64(), r.Float64())
+	s1 := e1 * r.Float64()
+	s2 := e2 * r.Float64()
+	if s1 > s2 {
+		return e1 - s1
+	} else {
+		return e2 - s2
+	}
+}
+
+func finalsdist(niter, nbucket int, r *rand.Rand) {
+	ch := make(chan []int)
+	for range runtime.NumCPU() {
+		go func(r *rand.Rand) {
+			dist := make([]int, nbucket)
+			for range niter {
+				dist[int(float64(nbucket)*finalsopponent(r))]++
+			}
+			ch <- dist
+		}(rand.New(rand.NewSource(int64(r.Uint64()))))
+	}
+	dist := make([]int, nbucket)
+	total := 0
+	for range runtime.NumCPU() {
+		for i, count := range <-ch {
+			dist[i] += count
+			total += count
+		}
+	}
+	sep := "line(["
+	for i, count := range dist {
+		fmt.Printf("%s(%f,%f)", sep, float64(i)/float64(nbucket), float64(count)/float64(total)*float64(nbucket))
+		sep = ","
+	}
+	fmt.Printf("])\n")
+}
+
 func main() {
 	const seed = 20260717
 	r := rand.New(rand.NewSource(seed))
@@ -123,5 +197,16 @@ func main() {
 		const niter = 100000
 		const nbucket = 40
 		ecsimulate(niter, nbucket, r)
+	}
+	{
+		const niter = 10000000
+		const nbucket = 100
+		semisdist(niter, nbucket, true, r)
+		semisdist(niter, nbucket, false, r)
+	}
+	{
+		const niter = 10000000
+		const nbucket = 100
+		finalsdist(niter, nbucket, r)
 	}
 }
